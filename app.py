8b2770884e5f099ad130df4681d8b6f53a050027
2DU <min08101@naver.com>
2017-02-15 16:16:10 +0900

    버그 패치

--- app.py
+++ app.py
@@ -139,8 +139,8 @@ def namumark(title, data):
         else:
             break
             
-    data = re.sub("<span>l</span><span>t</span><span>;</span>", "<span>lt;</span>", data)
-    data = re.sub("<span>g</span><span>t</span><span>;</span>", "<span>gt;</span>", data)
+    data = re.sub("<span>&</span><span>l</span><span>t</span><span>;</span>", "<span>&lt;</span>", data)
+    data = re.sub("<span>&</span><span>g</span><span>t</span><span>;</span>", "<span>&gt;</span>", data)
     
     data = re.sub("\[anchor\((?P<in>[^\[\]]*)\)\]", '<span id="\g<in>"></span>', data)
     data = re.sub('\[date\(now\)\]', getnow(), data)
