0d1f0a1a496e51192adc36641f09762dee8ebf45
ect (or 2du) <min08101@naver.com>
2017-07-24 12:46:07 +0900

    버그 수정

--- app.py
+++ app.py
@@ -1879,6 +1879,8 @@ def close_topic_list(name = None):
             ip = ip_pas(row[0]['ip'], 1)
                 
             div += '<h2><a href="/topic/' + url_pas(name) + '/sub/' + url_pas(data['sub']) + '">' + str((i + 1)) + '. ' + data['sub'] + '</a></h2><table id="toron"><tbody><tr><td id="toroncolorgreen"><a href="javascript:void(0);" id="1">#1</a> ' + ip + ' <span style="float:right;">' + row[0]['date'] + '</span></td></tr><tr><td ' + block + '>' + indata + '</td></tr></tbody></table><br>'
+            
+            i += 1
     else:
         div += '</div>'
         
