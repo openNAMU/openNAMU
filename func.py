df63134bba1382e7b1c45e555fd58de828521d2c
2DU <min08101@naver.com>
2017-11-04 17:03:57 +0900

    버그 패치

--- func.py
+++ func.py
@@ -130,7 +130,6 @@ def include_check(name, data):
             data = curs.fetchall()
             if(data):
                 curs.execute("delete from back where link = ?", [back_p[0]])
-                curs.execute("delete from cat where cat = ?", [back_p[0]])
                 namumark(back_p[0], data[0][0], 1, 0, 0)
 
 def ip_pas(raw_ip):
