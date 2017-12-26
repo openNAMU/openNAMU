b6fd9c3a9b0c18d6617c521fe8c88c6c3c72bf5b
2DU <min08101@naver.com>
2017-12-27 00:34:29 +0900

    수정

--- app.py
+++ app.py
@@ -3129,8 +3129,6 @@ def user_info():
         curs.execute("select ip from ok_login where ip = ?", [ip])
         if(curs.fetchall()):
             acl += ' (로그인 가능)'
-        
-    ip = ip_pas(conn, ip)
 
     custom_data = custom(conn)
     if(custom_data[2] != 0):
@@ -3140,21 +3138,22 @@ def user_info():
 
     return(html_minify(template('index', 
         imp = ['사용자 메뉴', wiki_set(conn, 1), custom_data, other2([0, 0])],
-        data =  ip + '<br><br>' + namumark(conn, '',    '== 상태 ==\r\n' + \
-                                                        '권한 상태 : ' + acl + '\r\n' + \
-                                                        '[목차(없음)]\r\n' + \
-                                                        '== 로그인 ==\r\n' + \
-                                                        plus + '\r\n' + \
-                                                        ' * [[wiki:register|회원가입]]\r\n' + \
-                                                        '== 사용자 기능 ==\r\n' + \
-                                                        ' * [[wiki:user_acl/' + url_pas(raw_ip) + '|사용자 문서 ACL]]\r\n' + \
-                                                        ' * [[wiki:custom_head|사용자 HEAD]]\r\n' + \
-                                                        '== 기타 ==\r\n' + \
-                                                        ' * [[wiki:alarm|알림]]\r\n' + \
-                                                        ' * [[wiki:view_log|지나온 문서]]\r\n' + \
-                                                        ' * [[wiki:record/' + raw_ip + '|편집 기록]]\r\n' + \
-                                                        ' * [[wiki:topic_record/' + raw_ip + '|토론 기록]]\r\n' + \
-                                                        ' * [[wiki:count|활동 횟수]]\r\n', 0, 0, 0),
+        data =  namumark(conn, '',  '[목차(없음)]\r\n' + \
+                                    '== 상태 ==\r\n' + \
+                                    '[[사용자:' + ip + '|' + ip + ']] [[wiki:record/' + url_pas(ip) + '|(기록)]]\r\n\r\n'
+                                    '권한 상태 : ' + acl + '\r\n' + \
+                                    '== 로그인 ==\r\n' + \
+                                    plus + '\r\n' + \
+                                    ' * [[wiki:register|회원가입]]\r\n' + \
+                                    '== 사용자 기능 ==\r\n' + \
+                                    ' * [[wiki:user_acl/' + url_pas(raw_ip) + '|사용자 문서 ACL]]\r\n' + \
+                                    ' * [[wiki:custom_head|사용자 HEAD]]\r\n' + \
+                                    '== 기타 ==\r\n' + \
+                                    ' * [[wiki:alarm|알림]]\r\n' + \
+                                    ' * [[wiki:view_log|지나온 문서]]\r\n' + \
+                                    ' * [[wiki:record/' + raw_ip + '|편집 기록]]\r\n' + \
+                                    ' * [[wiki:topic_record/' + raw_ip + '|토론 기록]]\r\n' + \
+                                    ' * [[wiki:count|활동 횟수]]\r\n', 0, 0, 0),
         menu = 0
     )))
 
