656320bdeb34a3a3b945cd28bd6368a8e5d05419
ect (or 2du) <min08101@naver.com>
2018-01-13 16:41:35 +0900

    v2.5.5 Beta

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ logging.basicConfig(level = logging.ERROR)
 session_opts = { 'session.type' : 'dbm', 'session.data_dir' : './app_session/', 'session.auto' : 1 }
 app = beaker.middleware.SessionMiddleware(app(), session_opts)
 BaseRequest.MEMFILE_MAX = 1000 ** 4
-r_ver = 'v2.5.4'
+r_ver = 'v2.5.5 Beta'
 
 from func import *
 from set_mark.mid_pas import mid_pas
