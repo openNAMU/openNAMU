fc6d75ce343f369880ff20e404608a8ad2c37bcd
ect (or 2du) <min08101@naver.com>
2018-02-23 18:30:08 +0900

    일부 버그 수정 및 간단화

--- set_mark/end.py
+++ set_mark/end.py
@@ -9,19 +9,11 @@ def end(data, category):
     data = re.sub("(\n#no-br#|#no-br#\n|#no-br#)", "", data)
     data = re.sub("&lt;space&gt;", " ", data)
 
-    com = re.compile('#base64#((?:(?!#\/base64#|%0A).)+)#\/base64#', re.DOTALL)
-    while 1:
-        m = com.search(data)
-        if m:
-            data = com.sub('<code>' + parse.unquote(m.groups()[0]).replace('&#95;', '_') + '</code>', data, 1)
-        else:
-            break
-
     com = re.compile('#base64#((?:(?!#\/base64#).)+)#\/base64#', re.DOTALL)
     while 1:
         m = com.search(data)
         if m:
-            data = com.sub('<pre style="margin-top: 10px;">' + parse.unquote(m.groups()[0]).replace('&#95;', '_').replace('\r\n', '<br>') + '</pre>', data, 1)
+            data = com.sub(parse.unquote(m.groups()[0]).replace('&#95;', '_').replace('\r\n', '<br>'), data, 1)
         else:
             break
 
@@ -41,22 +33,14 @@ def end(data, category):
     data = re.sub('<br><ul id="list">', '<ul id="list">', data)
     data = re.sub('<\/ul>\r<br>', '</ul>', data)
     data = re.sub('<\/table>\r<br><ul ', '</table><ul ', data)
-    data = re.sub('<pre style="margin-top: 10px;"><br>', '<pre style="margin-top: 10px;">', data)
-    data = re.sub('<\/pre>\r<br>', '</pre>', data)
     data = re.sub('<hr id="under_bar"([^>]*)>(\r)?<br>', '<hr id="under_bar" style="margin-top: -5px;">', data)
     data = re.sub('&lt;isbr&gt;', '\r\n', data)
     data = re.sub('^(?:<br>|\r|\n| )+', '', data)
     data = re.sub('^<div style="margin-top: 30px;" id="cate">', '<div id="cate">', data)        
     data = re.sub('&amp;#92;', '&#92;', data)
-    
-    data = data.replace('#mid#', '{{{')
-    data = data.replace('#/mid#', '}}}')
 
-    data = data.replace('#mid2#', '{{{')
-    data = data.replace('#/mid2#', '}}}')
-
-    if re.search('&lt;math&gt;((?:(?!&lt;\/math&gt;).)*)&lt;\/math&gt;', data):
-        data += '<script type="text/x-mathjax-config">MathJax.Hub.Config({ extensions: ["tex2jax.js", "AMSmath.js"], jax: ["input/TeX", "output/HTML-CSS"], tex2jax: { inlineMath: [ [\'<math>\',\'</math>\'] ],processEscapes: true }, }); </script>'
+    if re.search('\[math\]((?:(?!\[\/math\]).)*)\[\/math\]', data):
+        data += '<script type="text/x-mathjax-config">MathJax.Hub.Config({ extensions: ["tex2jax.js", "AMSmath.js"], jax: ["input/TeX", "output/HTML-CSS"], tex2jax: { inlineMath: [ [\'[math]\',\'[/math]\'] ],processEscapes: true }, }); </script>'
         data += '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.3/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>'
 
     return data
