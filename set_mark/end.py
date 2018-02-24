c6e632bb6777c9a7f4c0d1272ae46f0006f57746
ect (or 2du) <min08101@naver.com>
2018-02-24 17:14:53 +0900

    다시 되돌림

--- set_mark/end.py
+++ set_mark/end.py
@@ -13,7 +13,6 @@ def end(data, category):
     data = re.sub('<\/blockquote>(?:(?:\r)?\n)<br><blockquote>', '</blockquote><blockquote>', data)
 
     data = re.sub('\n', '<br>', data)
-    data = re.sub("\[br\]", '<br>', data)
 
     data = re.sub('<br><ul id="list">', '<ul id="list">', data)
     data = re.sub('<\/ul>\r<br>', '</ul>', data)
