265ba5300defecc6ec57c0135801d9e1dec7ed73
ect (or 2du) <min08101@naver.com>
2018-05-16 00:12:27 +0900

    한국어 데이터 추가

---
+++ language/test.py
@@ -0,0 +1,18 @@
+import json
+
+print('name : ', end = '')
+b = input()
+
+json_data = open('en-US.json', 'rt', encoding='utf-8').read()
+a_json = json.loads(json_data)
+
+json_data = open(str(b) + '.json', 'rt', encoding='utf-8').read()
+b_json = json.loads(json_data)
+
+for a_in in a_json:
+    if not a_in in b_json:
+        print(a_in + ' : ', end = '')
+        c = input()
+        b_json[a_in] = c
+        
+print(b_json)
