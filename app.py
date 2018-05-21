4574d40f1e11556a49d4ed55197c33d1b70a9be2
ect (or 2du) <min08101@naver.com>
2018-05-21 22:08:46 +0900

    이렇게 인가

--- app.py
+++ app.py
@@ -1072,9 +1072,8 @@ def update():
     if platform.system() == 'Linux':
         print('Update')
 
-        os.system('git remote rm origin')
         os.system('git remote add origin https://github.com/2DU/openNAMU.git')
-        ok = os.system('git pull origin ' + up_data)
+        ok = os.system('git reset --hard origin/' + up_data)
         if ok == 0:
             return redirect('/re_start')
         else:
