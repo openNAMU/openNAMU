d26270860cf151b1d2c7ddc48434a2306f585c83
ect (or 2du) <min08101@naver.com>
2018-05-21 22:18:41 +0900

    아 이건가!

--- app.py
+++ app.py
@@ -1074,7 +1074,7 @@ def update():
 
         os.system('git remote rm origin')
         os.system('git remote add origin https://github.com/2DU/openNAMU.git')
-        ok = os.system('git pull origin master')
+        ok = os.system('git pull origin ' + up_data)
         ok = os.system('git reset --hard origin/' + up_data)
         if ok == 0:
             return redirect('/re_start')
