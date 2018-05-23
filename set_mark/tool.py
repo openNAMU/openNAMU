847c2d1a4e4bd76483730afeb1d893ae81e0f6df
Surplus_Up (2DU) <min08101@naver.com>
2018-05-24 00:20:45 +0900

    혹시 모르니까

--- set_mark/tool.py
+++ set_mark/tool.py
@@ -12,7 +12,7 @@ def ip_check():
         ip = flask.session['DREAMER']
     else:
         try:
-            ip = flask.request.environ.get('HTTP_X_REAL_IP', flask.request.remote_addr)
+            ip = flask.request.environ.get('HTTP_X_REAL_IP', flask.request.environ.get('HTTP_X_FORWARDED_FOR', flask.request.remote_addr))
             
             if ip == ('::1' or '127.0.0.1'):
                 ip = flask.request.environ.get('HTTP_X_FORWARDED_FOR', flask.request.remote_addr)
