73c4454a9bad9a41ed18d9ce783d324892d3ddd0
Seia Soto <seriumium@outlook.kr>
2018-06-02 20:38:48 +0900

    Latest modifyed version

--- language/test.py
+++ language/test.py
@@ -15,4 +15,4 @@ for a_in in a_json:
         c = input()
         b_json[a_in] = c
         
-print(str(b_json).replace('", ', '",\n    ').replace('{', '{\n    ').replace('}', '\n}').replace('\'', '"'))
+print(str(b_json).replace(', ', ',\n    ').replace('{', '{\n    ').replace('}', '\n}').replace('\'', '"'))
