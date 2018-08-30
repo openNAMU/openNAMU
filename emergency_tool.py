2a16ba7a88f64057a6e692eb1658b041cede6b7e
Surplus_Up (2DU) <min08101@naver.com>
2018-08-30 23:41:25 +0900

    이머전시 툴에 포트 체인지 기능 추가

--- emergency_tool.py
+++ emergency_tool.py
@@ -1,55 +1,45 @@
-# 주요 모듈 불러옴
 import json
 import sqlite3
 import threading
 
-# 기타 코드 불러옴
 from func import *
 from mark import load_conn2, namumark
 
-# JSON 불러옴
 json_data = open('set.json').read()
 set_data = json.loads(json_data)
 
-# 디비 연결
 conn = sqlite3.connect(set_data['db'] + '.db', check_same_thread = False)
 curs = conn.cursor()
 
-# 연결 전달
 load_conn(conn)
 
-print('1. BackLink ReSet')
-print('2. ReCaptcha Delete')
-print('3. Ban Delete')
+print('1. backlink reset')
+print('2. reCAPTCHA delete')
+print('3. ban delete')
+print('4. change port')
 
 print('select : ', end = '')
 what_i_do = input()
 
 if what_i_do == '1':
-    # 파싱 해주는 함수
     def parser(data):
         namumark(data[0], data[1], 1)
 
-    # 역링크 전부 삭제
     curs.execute("delete from back")
     conn.commit()
 
-    # 데이터에서 제목이랑 내용 불러옴
     curs.execute("select title, data from data")
     data = curs.fetchall()
 
-    # for 돌려서 처리
     for test in data:
-        # 스레드 기반으로 처리
         t = threading.Thread(target = parser, args = [test])
         t.start()
         t.join()
 elif what_i_do == '2':
-    # 데이터 삭제
     curs.execute("delete from other where name = 'recaptcha'")
     curs.execute("delete from other where name = 'sec_re'")
 elif what_i_do == '3':
-    print('IP or User_Name : ', end = '')
+    print('ip or name : ', end = '')
     user_data = input()
 
     if re.search("^([0-9]{1,3}\.[0-9]{1,3})$", user_data):
@@ -57,11 +47,14 @@ elif what_i_do == '3':
     else:
         band = ''
 
-    # 데이터 삭제
-    curs.execute("insert into rb (block, end, today, blocker, why, band) values (?, ?, ?, ?, ?, ?)", [user_data, load_lang('release', 1), get_time(), load_lang('tool', 1) + ':Emergency', '', band])
+        curs.execute("insert into rb (block, end, today, blocker, why, band) values (?, ?, ?, ?, ?, ?)", [user_data, load_lang('release', 1), get_time(), load_lang('tool', 1) + ':emergency', '', band])
     curs.execute("delete from ban where block = ?", [user_data])
+elif what_i_do == '4':
+    print('port : ', end = '')
+    port = input()
 
-print('OK')
+    curs.execute("update other set data = ? where name = 'port'", [port])
 
-# 커밋
 conn.commit()
+
+print('ok')
