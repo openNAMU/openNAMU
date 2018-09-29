a384a057531f33c25cf3e037088247f8f4353e38
Surplus_Up (2DU) <min08101@naver.com>
2018-09-30 00:17:15 +0900

    임시 토론 갱신 적용

--- views/main_css/topic_reload.js
+++ views/main_css/topic_reload.js
@@ -1,34 +1,36 @@
-<div id="plus">
-</div>
-<script>
+function topic_load(name, sub) {
     function addZero(i) {
         if(i < 10) {
             i = "0" + i;
         }
+        
         return i;
     }
 
     setInterval(
         function() {
             var d = new Date();
-            var date = d.getFullYear() + '-' + addZero(d.getMonth() + 1) + '-' + d.getDate() + ' ' + addZero(d.getHours()) + ':' + addZero(d.getMinutes()) + ':' + addZero(d.getSeconds());
-
-            var url = "/api/topic/''' + name + '''/sub/''' + sub + '''";
-
-            var xhr = new XMLHttpRequest();
+            d.setSeconds(d.getSeconds() - 3);
             
-            xhr.open("GET", url);
-            xhr.send();
+            var date = d.getFullYear() + '-' + addZero(d.getMonth() + 1) + '-' + d.getDate() + ' ' + addZero(d.getHours()) + ':' + addZero(d.getMinutes()) + ':' + addZero(d.getSeconds());
+            var url = "/api/topic/" + name + "/sub/" + sub + "?time=" + date;
+            var xhr = new XMLHttpRequest();
+
+            doc_data = document.getElementById("plus");
+
+            test = '';
 
             xhr.onreadystatechange = function() {
                 if(xhr.status == 200) {
-                    var data = JSON.parse(xhr.responseText);
-
-                    data.forEach(function(element) {
-                        document.getElementById('plus').value += element['id'] + element['data'];
-                    });
+                    if(xhr.responseText != "{}\n" && test != xhr.responseText) {
+                        test = xhr.responseText;
+                        doc_data.innerText += xhr.responseText + '\n';
+                    }
                 }
             }
+
+            xhr.open("GET", url);
+            xhr.send();
         }
     , 3000);
-</script>
+}
