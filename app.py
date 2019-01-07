57c161bd81b76fcf0164371251f00c8df19df011
hoparkgo9ma <me@ho9.me>
2019-01-07 22:10:37 +0900

    0be3833 커밋으로 롤백, #588 은 issue-588 브랜치에서 계속 수정함
    넘쳐나는 버그때문에 일단 분리

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ import logging
 import random
 
 from func import *
-from views.easter_egg import *
 
 r_ver = 'v3.0.8-master-100'
 c_ver = ''.join(re.findall('[0-9]', r_ver))
@@ -270,11 +269,6 @@ adsense_result = curs.fetchall()
 if not adsense_result:
     curs.execute('insert into other (name, data) values ("adsense", "False")')
     curs.execute('insert into other (name, data) values ("adsense_code", "")')
-
-curs.execute('select data from other where name = "easter_egg"')
-sc_egg_result = curs.fetchall()
-if not sc_egg_result:
-    curs.execute('insert into other (name, data) values ("easter_egg", "True")')
 
 ask_this = [[['markup', 'markup'], ['namumark']], [['encryption method', 'encode'], ['sha256', 'sha3', 'bcrypt']]]
 for ask_data in ask_this:
@@ -2310,30 +2304,6 @@ def manager(num = 1):
         return easy_minify(flask.render_template(skin_check(), 
             imp = [load_lang('admin') + ' ' + load_lang('tool'), wiki_set(), custom(), other2([0, 0])],
             data =  '''
-                    <script>
-                    function config_easter_egg (target) {
-                        if (target.checked === true) {
-                            console.log('okay')
-                        }
-                        else {
-                            console.log('fucku')
-                        }
-                        
-                        $.ajax({
-                            type: 'POST',
-                            url : '/request/scrambled_eggs',
-                            data : {"config_easter_egg" : "True"},
-                            contentType : 'application/json',
-                            dataType : 'json',
-                            success : function(data) {
-                                console.log(data)
-                            },
-                            error : function(e) {
-                                console.log(e.responseText)
-                            }
-                        })
-                    }
-                    </script>
                     <h2>''' + load_lang('admin') + '''</h2>
                     <ul>
                         <li><a href="/manager/2">''' + load_lang('document') + ''' acl</a></li>
@@ -2348,7 +2318,7 @@ def manager(num = 1):
                         <li><a href="/manager/8">''' + load_lang('admin_group') + ' ' + load_lang('create') + '''</a></li>
                         <li><a href="/setting">''' + load_lang('setting') + ' ' + load_lang('edit') + '''</a></li>
                     </ul>
-                    <h2>''' + load_lang('filter') + '''</h2>
+                    <h3>''' + load_lang('filter') + '''</h3>
                     <ul>
                         <li><a href="/inter_wiki">''' + load_lang('interwiki') + '''</a></li>
                         <li><a href="/html_filter">html ''' + load_lang('filter') + '''</a></li>
@@ -2363,10 +2333,6 @@ def manager(num = 1):
                         <li><a href="/update">''' + load_lang('update') + '''</a></li>
                         <li><a href="/oauth_settings">''' + load_lang('oauth_settings') + '''</a></li>
                         <li><a href="/adsense_settings">''' + load_lang('adsense') + ' ' + load_lang('setting') + '''</a></li>
-                    </ul>
-                    <h2>''' + load_lang('other') + '''</h2>
-                    <ul>
-                        <li><label for="enable_easter_egg"><input type="checkbox" id="enable_easter_egg" name="enable_easter_egg" onChange="config_easter_egg(this)">이스터에그 활성화</label</li>
                     </ul>
                     ''',
             menu = [['other', load_lang('other')]]
@@ -4748,34 +4714,6 @@ def main_file(data = None):
         return flask.send_from_directory('./', data)
     else:
         return redirect('/w/' + url_pas(wiki_set(2)))
-
-@app.route('/request/egg')
-def request_egg():
-    return easter_egg[random.randint(0, len(easter_egg) - 1)]
-
-@app.route('/request/scrambled_eggs', methods=['POST'])
-def request_sc_egg():
-    #if admin_check(None, 'update') != 1:
-    #   return '{"request" : "err", "err_code" : "err_authority_not_enough"}'
-
-    #req = flask.request.form
-    #print(req)
-    #config = bool(flask.request.form.getlist('config_easter_egg'))
-
-    #curs.execute('update other set data = ? where name = "easter_egg"', [str(config)])
-    #conn.commit()
-
-    print(flask.request.headers['Content-Type'])
-    if flask.request.headers['Content-Type'] == 'application/json':
-        print(flask.request)
-        #print(flask.request.json())
-        print(flask.request.form.get('config_easter_egg'))
-
-    config = "True"
-    data = {'request' : "success", 'now' : config}
-    return str(data)
-    #else:
-    #    return '{"request" : "err", "err_code" : "err_no_data_required"}'
 
 @app.errorhandler(404)
 def error_404(e):
