64cc153446bd2b7ddf04fed20140c3b3c4b7704b
Surplus_Up (2DU) <min08101@naver.com>
2019-01-29 01:51:44 +0900

    흑우마가 잘못한 번역 수정

--- func.py
+++ func.py
@@ -251,24 +251,24 @@ def ip_or_user(data):
 def edit_help_button():
     # https://stackoverflow.com/questions/11076975/insert-text-into-textarea-at-cursor-position-javascript
     js_data =   '''
-                <script>
-                    function insert_data(name, data) {
-                        if(document.selection) { 
-                            document.getElementById(name).focus();
+        <script>
+            function insert_data(name, data) {
+                if(document.selection) { 
+                    document.getElementById(name).focus();
 
-                            sel = document.selection.createRange();
-                            sel.text = data; 
-                        } else if(document.getElementById(name).selectionStart || document.getElementById(name).selectionStart == '0') {
-                            var startPos = document.getElementById(name).selectionStart;
-                            var endPos = document.getElementById(name).selectionEnd;
+                    sel = document.selection.createRange();
+                    sel.text = data; 
+                } else if(document.getElementById(name).selectionStart || document.getElementById(name).selectionStart == '0') {
+                    var startPos = document.getElementById(name).selectionStart;
+                    var endPos = document.getElementById(name).selectionEnd;
 
-                            document.getElementById(name).value = document.getElementById(name).value.substring(0, startPos) + data + document.getElementById(name).value.substring(endPos, document.getElementById(name).value.length); 
-                        } else {
-                            document.getElementById(name).value += data;
-                        }
-                    }
-                </script>
-                '''
+                    document.getElementById(name).value = document.getElementById(name).value.substring(0, startPos) + data + document.getElementById(name).value.substring(endPos, document.getElementById(name).value.length); 
+                } else {
+                    document.getElementById(name).value += data;
+                }
+            }
+        </script>
+    '''
 
     insert_list = [['[[|]]', '[[|]]'], ['[*()]', '[*()]'], ['{{{#!}}}', '{{{#!}}}'], ['||<>||', '||<>||'], ["\\'\\'\\'", "\'\'\'"]]
 
