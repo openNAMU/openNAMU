3a8af11502d87af0ac204cb203902ae19c2f7965
Surplus_UP (2DU) <min08101@naver.com>
2019-02-15 04:00:10 +0900

    일부 기능 수정, 인터위키 코드 분리, 버그 수정

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from route.read_view import *
 from route.delete import *
 from route.del_alarm import *
 from route.alarm import *
+from route.inter_wiki import *
 
 r_ver = 'v3.0.9-master-004'
 c_ver = '309001'
@@ -327,76 +328,7 @@ def alarm():
 
 @app.route('/<regex("inter_wiki|(?:edit|email|name)_filter"):tools>')
 def inter_wiki(tools = None):
-    div = ''
-    admin = admin_check()
-
-    if tools == 'inter_wiki':
-        del_link = 'del_inter_wiki'
-        plus_link = 'plus_inter_wiki'
-        title = load_lang('interwiki_list')
-        div = ''
-
-        curs.execute('select title, link from inter')
-    elif tools == 'email_filter':
-        del_link = 'del_email_filter'
-        plus_link = 'plus_email_filter'
-        title = load_lang('email_filter_list')
-        div =   '''
-                <ul>
-                    <li>gmail.com</li>
-                    <li>naver.com</li>
-                    <li>daum.net</li>
-                    <li>hanmail.net</li>
-                    <li>hanmail2.net</li>
-                </ul>
-                '''
-
-        curs.execute("select html from html_filter where kind = 'email'")
-    elif tools == 'name_filter':
-        del_link = 'del_name_filter'
-        plus_link = 'plus_name_filter'
-        title = load_lang('id_filter_list')
-        div = ''
-
-        curs.execute("select html from html_filter where kind = 'name'")
-    else:
-        del_link = 'del_edit_filter'
-        plus_link = 'manager/9'
-        title = load_lang('edit_filter_list')
-        div = ''
-
-        curs.execute("select name from filter")
-
-    db_data = curs.fetchall()
-    if db_data:
-        div += '<ul>'
-
-        for data in db_data:
-            if tools == 'inter_wiki':
-                div += '<li>' + data[0] + ' : <a id="out_link" href="' + data[1] + '">' + data[1] + '</a>'
-            elif tools == 'edit_filter':
-                div += '<li><a href="/plus_edit_filter/' + url_pas(data[0]) + '">' + data[0] + '</a>'
-            else:
-                div += '<li>' + data[0]
-
-            if admin == 1:
-                div += ' <a href="/' + del_link + '/' + url_pas(data[0]) + '">(' + load_lang('delete') + ')</a>'
-
-            div += '</li>'
-
-        div += '</ul>'
-
-        if admin == 1:
-            div += '<hr class=\"main_hr\"><a href="/' + plus_link + '">(' + load_lang('add') + ')</a>'
-    else:
-        if admin == 1:
-            div += '<a href="/' + plus_link + '">(' + load_lang('add') + ')</a>'
-
-    return easy_minify(flask.render_template(skin_check(), 
-        imp = [title, wiki_set(), custom(), other2([0, 0])],
-        data = div,
-        menu = [['other', load_lang('return')]]
-    ))
+    return inter_wiki_2(conn, tools)
 
 @app.route('/<regex("del_(?:inter_wiki|(?:edit|email|name)_filter)"):tools>/<name>')
 def del_inter(tools = None, name = None):
@@ -785,7 +717,7 @@ def setting(num = 0):
                 data = ''
 
             return easy_minify(flask.render_template(skin_check(), 
-                imp = [html.unescape(load_lang('main' + title)), wiki_set(), custom(), other2([0, 0])],
+                imp = [load_lang(data = 'main' + title, safe = 1), wiki_set(), custom(), other2([0, 0])],
                 data =  '''
                         <form method="post">
                             <textarea rows="25" name="content">''' + html.escape(data) + '''</textarea>
@@ -4327,7 +4259,7 @@ def custom_head_view():
         start += '<span>&lt;style&gt;css&lt;/style&gt;<br>&lt;script&gt;js&lt;/script&gt;</span><hr class=\"main_hr\">'
 
         return easy_minify(flask.render_template(skin_check(), 
-            imp = [html.unescape(load_lang('user_head')), wiki_set(), custom(), other2([0, 0])],
+            imp = [load_lang(data = 'user_head', safe = 1), wiki_set(), custom(), other2([0, 0])],
             data =  start + '''
                     <form method="post">
                         <textarea rows="25" cols="100" name="content">''' + data + '''</textarea>
