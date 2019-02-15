00624f88f43bcbc3a35866678b99375034c5d67f
Surplus_UP (2DU) <min08101@naver.com>
2019-02-16 02:54:52 +0900

    하나 더 분리하고 잔다

--- app.py
+++ app.py
@@ -350,20 +350,7 @@ def setting(num = 0):
 
 @app.route('/not_close_topic')
 def not_close_topic():
-    div = '<ul>'
-    
-    curs.execute('select title, sub from rd where stop != "O" order by date desc')
-    n_list = curs.fetchall()
-    for data in n_list:
-        div += '<li><a href="/topic/' + url_pas(data[0]) + '/sub/' + url_pas(data[1]) + '">' + html.escape(data[0]) + ' (' + data[1] + ')</a></li>'
-            
-    div += '</ul>'
-
-    return easy_minify(flask.render_template(skin_check(), 
-        imp = [load_lang('open_discussion_list'), wiki_set(), custom(), other2([0, 0])],
-        data = div,
-        menu = [['manager', load_lang('return')]]
-    ))
+    return not_close_topic_2(conn)
 
 @app.route('/image/<name>')
 def image_view(name = None):
