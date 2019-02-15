a71d21df2aee9a0f18d1f20a7e6db7f830a2fb08
Surplus_UP (2DU) <min08101@naver.com>
2019-02-16 02:33:02 +0900

    코드로 import 하게 변경 및 2개 더 분리

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from route.read_view import *
-from route.delete import *
-from route.del_alarm import *
-from route.alarm import *
-from route.inter_wiki import *
+import os
+import re
+
+for i_data in os.listdir("route"):
+    f_src = re.search("(.+)\.py$", i_data)
+    if f_src:
+        f_src = f_src.groups()[0]
+
+        exec("from route." + f_src + " import *")
 
 r_ver = 'v3.0.9-master-004'
 c_ver = '309001'
@@ -332,112 +336,12 @@ def inter_wiki(tools = None):
 
 @app.route('/<regex("del_(?:inter_wiki|(?:edit|email|name)_filter)"):tools>/<name>')
 def del_inter(tools = None, name = None):
-    if admin_check(None, tools) == 1:
-        if tools == 'del_inter_wiki':
-            curs.execute("delete from inter where title = ?", [name])
-        elif tools == 'del_edit_filter':
-            curs.execute("delete from filter where name = ?", [name])
-        elif tools == 'del_name_filter':
-            curs.execute("delete from html_filter where html = ? and kind = 'name'", [name])
-        else:
-            curs.execute("delete from html_filter where html = ? and kind = 'email'", [name])
-        
-        conn.commit()
-
-        return redirect('/' + re.sub('^del_', '', tools))
-    else:
-        return re_error('/error/3')
+    return del_inter_2(conn, tools, name)
 
 @app.route('/<regex("plus_(?:inter_wiki|(?:edit|email|name)_filter)"):tools>', methods=['POST', 'GET'])
 @app.route('/<regex("plus_edit_filter"):tools>/<name>', methods=['POST', 'GET'])
 def plus_inter(tools = None, name = None):
-    if flask.request.method == 'POST':
-        if tools == 'plus_inter_wiki':
-            curs.execute('insert into inter (title, link) values (?, ?)', [flask.request.form.get('title', None), flask.request.form.get('link', None)])
-            admin_check(None, 'inter_wiki_plus')
-        elif tools == 'plus_edit_filter':
-            if admin_check(1, 'edit_filter edit') != 1:
-                return re_error('/error/3')
-
-            if flask.request.form.get('limitless', '') != '':
-                end = 'X'
-            else:
-                end = flask.request.form.get('second', 'X')
-
-            curs.execute("select name from filter where name = ?", [name])
-            if curs.fetchall():
-                curs.execute("update filter set regex = ?, sub = ? where name = ?", [flask.request.form.get('content', 'test'), end, name])
-            else:
-                curs.execute("insert into filter (name, regex, sub) values (?, ?, ?)", [name, flask.request.form.get('content', 'test'), end])
-        else:
-            if tools == 'plus_name_filter':
-                admin_check(None, 'name_filter edit')
-                type_d = 'name'
-            else:
-                admin_check(None, 'email_filter edit')
-                type_d = 'email'
-            
-            curs.execute('insert into html_filter (html, kind) values (?, ?)', [flask.request.form.get('title', 'test'), type_d])
-        
-        conn.commit()
-    
-        return redirect('/' + re.sub('^plus_', '', tools))
-    else:
-        if admin_check(1) != 1:
-            stat = 'disabled'
-        else:
-            stat = ''
-
-        if tools == 'plus_inter_wiki':
-            title = load_lang('interwiki_add')
-            form_data = '''
-                        <input placeholder="''' + load_lang('name') + '''" type="text" name="title">
-                        <hr class=\"main_hr\">
-                        <input placeholder="link" type="text" name="link">
-                        '''
-        elif tools == 'plus_edit_filter':
-            curs.execute("select regex, sub from filter where name = ?", [name])
-            exist = curs.fetchall()
-            if exist:
-                textarea = exist[0][0]
-                
-                if exist[0][1] == 'X':
-                    time_check = 'checked="checked"'
-                    time_data = ''
-                else:
-                    time_check = ''
-                    time_data = exist[0][1]
-            else:
-                textarea = ''
-                time_check = ''
-                time_data = ''
-
-            title = load_lang('edit_filter_add')
-            form_data = '''
-                        <input placeholder="''' + load_lang('second') + '''" name="second" type="text" value="''' + html.escape(time_data) + '''">
-                        <hr class=\"main_hr\">
-                        <input ''' + stat + ''' type="checkbox" ''' + time_check + ''' name="limitless"> ''' + load_lang('limitless') + '''
-                        <hr class=\"main_hr\">
-                        <input ''' + stat + ''' placeholder="''' + load_lang('regex') + '''" name="content" value="''' + html.escape(textarea) + '''" type="text">
-                        '''
-        elif tools == 'plus_name_filter':
-            title = load_lang('id_filter_add')
-            form_data = '<input placeholder="' + load_lang('id') + ' ' + load_lang('regex') + '" type="text" name="title">'
-        else:
-            title = load_lang('email_filter_add')
-            form_data = '<input placeholder="email" type="text" name="title">'
-
-        return easy_minify(flask.render_template(skin_check(), 
-            imp = [title, wiki_set(), custom(), other2([0, 0])],
-            data =  '''
-                    <form method="post">
-                        ''' + form_data + '''
-                        <hr class=\"main_hr\">
-                        <button ''' + stat + ''' type="submit">''' + load_lang('add') + '''</button>
-                    </form>
-                    ''',
-            menu = [[re.sub('^plus_', '', tools), load_lang('return')]]
-        ))
+    return plus_inter_2(conn, tools, name)
 
 @app.route('/setting')
 @app.route('/setting/<int:num>', methods=['POST', 'GET'])
---
+++ route/plus_inter.py
@@ -0,0 +1,92 @@
+from .tool.func import *
+
+def plus_inter_2(conn, tools, name):
+    curs = conn.cursor()
+    
+    if flask.request.method == 'POST':
+        if tools == 'plus_inter_wiki':
+            curs.execute('insert into inter (title, link) values (?, ?)', [flask.request.form.get('title', None), flask.request.form.get('link', None)])
+            admin_check(None, 'inter_wiki_plus')
+        elif tools == 'plus_edit_filter':
+            if admin_check(1, 'edit_filter edit') != 1:
+                return re_error('/error/3')
+
+            if flask.request.form.get('limitless', '') != '':
+                end = 'X'
+            else:
+                end = flask.request.form.get('second', 'X')
+
+            curs.execute("select name from filter where name = ?", [name])
+            if curs.fetchall():
+                curs.execute("update filter set regex = ?, sub = ? where name = ?", [flask.request.form.get('content', 'test'), end, name])
+            else:
+                curs.execute("insert into filter (name, regex, sub) values (?, ?, ?)", [name, flask.request.form.get('content', 'test'), end])
+        else:
+            if tools == 'plus_name_filter':
+                admin_check(None, 'name_filter edit')
+                type_d = 'name'
+            else:
+                admin_check(None, 'email_filter edit')
+                type_d = 'email'
+            
+            curs.execute('insert into html_filter (html, kind) values (?, ?)', [flask.request.form.get('title', 'test'), type_d])
+        
+        conn.commit()
+    
+        return redirect('/' + re.sub('^plus_', '', tools))
+    else:
+        if admin_check(1) != 1:
+            stat = 'disabled'
+        else:
+            stat = ''
+
+        if tools == 'plus_inter_wiki':
+            title = load_lang('interwiki_add')
+            form_data = '''
+                        <input placeholder="''' + load_lang('name') + '''" type="text" name="title">
+                        <hr class=\"main_hr\">
+                        <input placeholder="link" type="text" name="link">
+                        '''
+        elif tools == 'plus_edit_filter':
+            curs.execute("select regex, sub from filter where name = ?", [name])
+            exist = curs.fetchall()
+            if exist:
+                textarea = exist[0][0]
+                
+                if exist[0][1] == 'X':
+                    time_check = 'checked="checked"'
+                    time_data = ''
+                else:
+                    time_check = ''
+                    time_data = exist[0][1]
+            else:
+                textarea = ''
+                time_check = ''
+                time_data = ''
+
+            title = load_lang('edit_filter_add')
+            form_data = '''
+                        <input placeholder="''' + load_lang('second') + '''" name="second" type="text" value="''' + html.escape(time_data) + '''">
+                        <hr class=\"main_hr\">
+                        <input ''' + stat + ''' type="checkbox" ''' + time_check + ''' name="limitless"> ''' + load_lang('limitless') + '''
+                        <hr class=\"main_hr\">
+                        <input ''' + stat + ''' placeholder="''' + load_lang('regex') + '''" name="content" value="''' + html.escape(textarea) + '''" type="text">
+                        '''
+        elif tools == 'plus_name_filter':
+            title = load_lang('id_filter_add')
+            form_data = '<input placeholder="' + load_lang('id') + ' ' + load_lang('regex') + '" type="text" name="title">'
+        else:
+            title = load_lang('email_filter_add')
+            form_data = '<input placeholder="email" type="text" name="title">'
+
+        return easy_minify(flask.render_template(skin_check(), 
+            imp = [title, wiki_set(), custom(), other2([0, 0])],
+            data =  '''
+                    <form method="post">
+                        ''' + form_data + '''
+                        <hr class=\"main_hr\">
+                        <button ''' + stat + ''' type="submit">''' + load_lang('add') + '''</button>
+                    </form>
+                    ''',
+            menu = [[re.sub('^plus_', '', tools), load_lang('return')]]
+        ))
