39081241db3ea0abac404dbdb2453288c4606c16
잉여강화기 (SPUP) <min08101@naver.com>
2019-02-21 22:29:08 +0900

    라우터 분리한 파일 추가

---
+++ route/manager.py
@@ -0,0 +1,83 @@
+from .tool.func import *
+
+def manager_2(conn, num):
+    curs = conn.cursor()
+    
+    title_list = {
+        0 : [load_lang('document_name'), 'acl'], 
+        1 : [0, 'check'], 
+        2 : [0, 'ban'], 
+        3 : [0, 'admin'], 
+        4 : [0, 'record'], 
+        5 : [0, 'topic_record'], 
+        6 : [load_lang('name'), 'admin_plus'], 
+        7 : [load_lang('name'), 'plus_edit_filter'], 
+        8 : [load_lang('document_name'), 'search'], 
+        9 : [0, 'block_user'], 
+        10 : [0, 'block_admin'], 
+        11 : [load_lang('document_name'), 'watch_list'], 
+        12 : [load_lang('compare_target'), 'check'], 
+        13 : [load_lang('document_name'), 'edit']
+    }
+    
+    if num == 1:
+        return easy_minify(flask.render_template(skin_check(), 
+            imp = [load_lang('admin_tool'), wiki_set(), custom(), other2([0, 0])],
+            data =  '''
+                    <h2>''' + load_lang('admin') + '''</h2>
+                    <ul>
+                        <li><a href="/manager/2">''' + load_lang('acl_document_list') + '''</a></li>
+                        <li><a href="/manager/3">''' + load_lang('check_user') + '''</a></li>
+                        <li><a href="/manager/4">''' + load_lang('ban') + '''</a></li>
+                        <li><a href="/manager/5">''' + load_lang('authorize') + '''</a></li>
+                        <li><a href="/edit_filter">''' + load_lang('edit_filter_list') + '''</a></li>
+                    </ul>
+                    <br>
+                    <h2>''' + load_lang('owner') + '''</h2>
+                    <ul>
+                        <li><a href="/manager/8">''' + load_lang('admin_group_add') + '''</a></li>
+                        <li><a href="/setting">''' + load_lang('setting') + '''</a></li>
+                    </ul>
+                    <h3>''' + load_lang('filter') + '''</h3>
+                    <ul>
+                        <li><a href="/inter_wiki">''' + load_lang('interwiki_list') + '''</a></li>
+                        <li><a href="/email_filter">''' + load_lang('email_filter_list') + '''</a></li>
+                        <li><a href="/name_filter">''' + load_lang('id_filter_list') + '''</a></li>
+                    </ul>
+                    <br>
+                    <h2>''' + load_lang('server') + '''</h2>
+                    <ul>
+                        <li><a href="/indexing">''' + load_lang('indexing') + '''</a></li>
+                        <li><a href="/restart">''' + load_lang('wiki_restart') + '''</a></li>
+                        <li><a href="/update">''' + load_lang('update') + '''</a></li>
+                        <li><a href="/oauth_setting">''' + load_lang('oauth_setting') + '''</a></li>
+                        <li><a href="/adsense_setting">''' + load_lang('adsense_setting') + '''</a></li>
+                    </ul>
+                    ''',
+            menu = [['other', load_lang('return')]]
+        ))
+    elif not num - 1 > len(title_list):
+        if flask.request.method == 'POST':
+            if flask.request.args.get('plus', None):
+                return redirect('/' + title_list[(num - 2)][1] + '/' + url_pas(flask.request.args.get('plus', None)) + '?plus=' + flask.request.form.get('name', None))
+            else:
+                return redirect('/' + title_list[(num - 2)][1] + '/' + url_pas(flask.request.form.get('name', None)))
+        else:
+            if title_list[(num - 2)][0] == 0:
+                placeholder = load_lang('user_name')
+            else:
+                placeholder = title_list[(num - 2)][0]
+
+            return easy_minify(flask.render_template(skin_check(), 
+                imp = ['Redirect', wiki_set(), custom(), other2([0, 0])],
+                data =  '''
+                        <form method="post">
+                            <input placeholder="''' + placeholder + '''" name="name" type="text">
+                            <hr class=\"main_hr\">
+                            <button type="submit">''' + load_lang('go') + '''</button>
+                        </form>
+                        ''',
+                menu = [['manager', load_lang('return')]]
+            ))
+    else:
+        return redirect()
---
+++ route/topic.py
@@ -0,0 +1,201 @@
+from .tool.func import *
+
+def topic_2(conn, name, sub):
+    curs = conn.cursor()
+    
+    ban = topic_check(name, sub)
+    admin = admin_check(3)
+    
+    if flask.request.method == 'POST':
+        if captcha_post(flask.request.form.get('g-recaptcha-response', '')) == 1:
+            return re_error('/error/13')
+        else:
+            captcha_post('', 0)
+
+        ip = ip_check()
+        today = get_time()
+        
+        if ban == 1:
+            return re_error('/ban')
+        
+        curs.execute("select id from topic where title = ? and sub = ? order by id + 0 desc limit 1", [name, sub])
+        old_num = curs.fetchall()
+        if old_num:
+            num = int(old_num[0][0]) + 1
+        else:
+            num = 1
+
+        match = re.search('^user:([^/]+)', name)
+        if match:
+            curs.execute('insert into alarm (name, data, date) values (?, ?, ?)', [match.groups()[0], ip + ' - <a href="/topic/' + url_pas(name) + '/sub/' + url_pas(sub) + '">' + load_lang('user_discussion', 1) + '</a>', today])
+        
+        data = re.sub('\[\[((?:분류|category):(?:(?:(?!\]\]).)*))\]\]', '[br]', flask.request.form.get('content', None))
+        for rd_data in re.findall("(?:#([0-9]+))", data):
+            curs.execute("select ip from topic where title = ? and sub = ? and id = ?", [name, sub, rd_data])
+            ip_data = curs.fetchall()
+            if ip_data and ip_or_user(ip_data[0][0]) == 0:
+                curs.execute('insert into alarm (name, data, date) values (?, ?, ?)', [ip_data[0][0], ip + ' - <a href="/topic/' + url_pas(name) + '/sub/' + url_pas(sub) + '#' + str(num) + '">' + load_lang('discussion', 1) + '</a>', today])
+            
+        data = re.sub("(?P<in>#(?:[0-9]+))", '[[\g<in>]]', data)
+
+        data = savemark(data)
+
+        rd_plus(name, sub, today)
+
+        curs.execute("insert into topic (id, title, sub, data, date, ip, block, top) values (?, ?, ?, ?, ?, ?, '', '')", [str(num), name, sub, data, today, ip])
+        conn.commit()
+        
+        return redirect('/topic/' + url_pas(name) + '/sub/' + url_pas(sub) + '#reload')
+    else:
+        curs.execute("select title from rd where title = ? and sub = ? and stop = 'O'", [name, sub])
+        close_data = curs.fetchall()
+        
+        curs.execute("select title from rd where title = ? and sub = ? and stop = 'S'", [name, sub])
+        stop_data = curs.fetchall()
+        
+        curs.execute("select id from topic where title = ? and sub = ? limit 1", [name, sub])
+        topic_exist = curs.fetchall()
+        
+        display = ''
+        all_data = ''
+        data = ''
+        number = 1
+        
+        if admin == 1 and topic_exist:
+            if close_data:
+                all_data += '<a href="/topic/' + url_pas(name) + '/sub/' + url_pas(sub) + '/tool/close">(' + load_lang('open') + ')</a> '
+            else:
+                all_data += '<a href="/topic/' + url_pas(name) + '/sub/' + url_pas(sub) + '/tool/close">(' + load_lang('close') + ')</a> '
+            
+            if stop_data:
+                all_data += '<a href="/topic/' + url_pas(name) + '/sub/' + url_pas(sub) + '/tool/stop">(' + load_lang('restart') + ')</a> '
+            else:
+                all_data += '<a href="/topic/' + url_pas(name) + '/sub/' + url_pas(sub) + '/tool/stop">(' + load_lang('stop') + ')</a> '
+
+            curs.execute("select title from rd where title = ? and sub = ? and agree = 'O'", [name, sub])
+            if curs.fetchall():
+                all_data += '<a href="/topic/' + url_pas(name) + '/sub/' + url_pas(sub) + '/tool/agree">(' + load_lang('destruction') + ')</a>'
+            else:
+                all_data += '<a href="/topic/' + url_pas(name) + '/sub/' + url_pas(sub) + '/tool/agree">(' + load_lang('agreement') + ')</a>'
+            
+            all_data += '<hr class=\"main_hr\">'
+        
+        if (close_data or stop_data) and admin != 1:
+            display = 'display: none;'
+        
+        curs.execute("select data, id, date, ip, block, top from topic where title = ? and sub = ? order by id + 0 asc", [name, sub])
+        topic = curs.fetchall()
+        
+        curs.execute("select data, id, date, ip from topic where title = ? and sub = ? and top = 'O' order by id + 0 asc", [name, sub])
+        for topic_data in curs.fetchall():                   
+            who_plus = ''
+            
+            curs.execute("select who from re_admin where what = ? order by time desc limit 1", ['notice (' + name + ' - ' + sub + '#' + topic_data[1] + ')'])
+            topic_data_top = curs.fetchall()
+            if topic_data_top:
+                who_plus += ' <span style="margin-right: 5px;">@' + topic_data_top[0][0] + ' </span>'
+                                
+            all_data += '''
+                        <table id="toron">
+                            <tbody>
+                                <tr>
+                                    <td id="toron_color_red">
+                                        <a href="#''' + topic_data[1] + '''">
+                                            #''' + topic_data[1] + '''
+                                        </a> ''' + ip_pas(topic_data[3]) + who_plus + ''' <span style="float: right;">''' + topic_data[2] + '''</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>''' + render_set(data = topic_data[0]) + '''</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <br>
+                        '''    
+
+        for topic_data in topic:
+            user_write = topic_data[0]
+
+            if number == 1:
+                start = topic_data[3]
+
+            if topic_data[4] == 'O':
+                blind_data = 'id="toron_color_grey"'
+                
+                if admin != 1:
+                    curs.execute("select who from re_admin where what = ? order by time desc limit 1", ['blind (' + name + ' - ' + sub + '#' + str(number) + ')'])
+                    who_blind = curs.fetchall()
+                    if who_blind:
+                        user_write = '[[user:' + who_blind[0][0] + ']] ' + load_lang('hide')
+                    else:
+                        user_write = load_lang('hide')
+            else:
+                blind_data = ''
+
+            user_write = render_set(data = user_write)
+            ip = ip_pas(topic_data[3])
+            
+            curs.execute('select acl from user where id = ?', [topic_data[3]])
+            user_acl = curs.fetchall()
+            if user_acl and user_acl[0][0] != 'user':
+                ip += ' <a href="javascript:void(0);" title="' + load_lang('admin') + '">★</a>'
+
+            if admin == 1 or blind_data == '':
+                ip += ' <a href="/topic/' + url_pas(name) + '/sub/' + url_pas(sub) + '/admin/' + str(number) + '">(' + load_lang('discussion_tool') + ')</a>'
+
+            curs.execute("select end from ban where block = ?", [topic_data[3]])
+            if curs.fetchall():
+                ip += ' <a href="javascript:void(0);" title="' + load_lang('blocked') + '">†</a>'
+                    
+            if topic_data[5] == '1':
+                color = '_blue'
+            elif topic_data[3] == start:
+                color = '_green'
+            else:
+                color = ''
+                
+            if user_write == '':
+                user_write = '<br>'
+                         
+            all_data += '''
+                        <table id="toron">
+                            <tbody>
+                                <tr>
+                                    <td id="toron_color''' + color + '''">
+                                        <a href="javascript:void(0);" id="''' + str(number) + '">#' + str(number) + '</a> ' + ip + '''</span>
+                                    </td>
+                                </tr>
+                                <tr ''' + blind_data + '''>
+                                    <td>''' + user_write + '''</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <br>
+                        '''
+            number += 1
+
+        if ban != 1 or admin == 1:
+            data += '''
+                    <div id="plus"></div>
+                    <script type="text/javascript" src="/views/main_css/topic_reload.js"></script>
+                    <script>topic_load("''' + name + '''", "''' + sub + '''");</script>
+                    <a id="reload" href="javascript:void(0);" onclick="location.href.endsWith(\'#reload\')? location.reload(true):location.href=\'#reload\'">(''' + load_lang('reload') + ''')</a>
+                    <form style="''' + display + '''" method="post">
+                    <br>
+                    <textarea style="height: 100px;" name="content"></textarea>
+                    <hr class=\"main_hr\">
+                    ''' + captcha_get()
+            
+            if display == '':
+                data += ip_warring()
+
+            data += '''
+                        <button type="submit">''' + load_lang('send') + '''</button>
+                    </form>
+                    '''
+
+        return easy_minify(flask.render_template(skin_check(), 
+            imp = [name, wiki_set(), custom(), other2([' (' + load_lang('discussion') + ')', 0])],
+            data = '<h2 id="topic_top_title">' + sub + '</h2>' + all_data + data,
+            menu = [['topic/' + url_pas(name), load_lang('list')]]
+        ))
