cdaa396b8c9d935a38ae0221b4584ba8bd17d0af
Surplus_Up (2DU) <min08101@naver.com>
2019-02-26 18:14:32 +0900

    업데이트에 버튼 추가

--- route/restart.py
+++ route/restart.py
@@ -3,14 +3,16 @@ from .tool.func import *
 def restart_2(conn):
     curs = conn.cursor()
 
-    if admin_check(None, 'restart') != 1:
+    if admin_check() != 1:
         return re_error('/error/3')
 
     if flask.request.method == 'POST':
-        os.execl(sys.executable, sys.executable, *sys.argv)
-    else:
+        admin_check(None, 'restart')
+
         print('Restart')
 
+        os.execl(sys.executable, sys.executable, *sys.argv)
+    else:
         return easy_minify(flask.render_template(skin_check(), 
             imp = [load_lang('wiki_restart'), wiki_set(), custom(), other2([0, 0])],
             data =  '''
