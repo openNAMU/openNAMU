ecf0128ca78b58d2124314183ce683bf5f989a9c
Surplus_Up (2DU) <min08101@naver.com>
2019-03-02 23:51:12 +0900

    아 이렇게 해보자

--- route/change_password.py
+++ route/change_password.py
@@ -86,7 +86,8 @@ def change_password_2(conn, server_init):
             
             oauth_content += '</ul>'
 
-            if not re.search('^https:\/\/', flask.request.host_url):
+            forwarded_protocol = request.headers.get('X-Forwarded-Proto', None)
+            if forwarded_protocol == 'http':
                 http_warring = '<hr class=\"main_hr\"><span>' + load_lang('http_warring') + '</span>'
             else:
                 http_warring = ''
