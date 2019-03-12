3c0d7141fd46fa50ffe9c1cc4c06e6d13bc802fe
Surplus_Up (2DU) <min08101@naver.com>
2019-03-13 01:38:04 +0900

    문구 버그 수정

--- route/register.py
+++ route/register.py
@@ -89,7 +89,7 @@ def register_2(conn):
             contract = data[0][0] + '<hr class=\"main_hr\">'
         
         forwarded_protocol = flask.request.headers.get('X-Forwarded-Proto', None)
-        if forwarded_protocol == 'http':
+        if not forwarded_protocol or forwarded_protocol == 'http':
             http_warring = '<hr class=\"main_hr\"><span>' + load_lang('http_warring') + '</span>'
         else:
             http_warring = ''
