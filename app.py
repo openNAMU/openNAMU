94b40236283f8f31044c77e274d142be82ba8391
Surplus_Up (2DU) <min08101@naver.com>
2019-03-31 01:57:14 +0900

    버전 업

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ for i_data in os.listdir("route"):
 
         exec("from route." + f_src + " import *")
 
-r_ver = 'v3.1.0-master-01'
+r_ver = 'v3.1.0-master-02'
 c_ver = '400000'
 
 print('Version : ' + r_ver)
