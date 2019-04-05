11a4c8fb31f12ad010aca797ee6239778187e46d
Surplus_Up (2DU) <min08101@naver.com>
2019-04-06 02:11:40 +0900

    스킨 설정에 언어 연동되게

--- route/logout.py
+++ route/logout.py
@@ -3,7 +3,7 @@ from .tool.func import *
 def logout_2(conn):
     curs = conn.cursor()
 
-    flask.session['state'] = 0
+    flask.session.pop('state', None)
     flask.session.pop('id', None)
 
     return redirect('/user')
--- views/neo_yousoro/js/skin_set.js
+++ views/neo_yousoro/js/skin_set.js
@@ -65,8 +65,38 @@ main_load();
 
 window.onload = function () {
     if(window.location.pathname === '/skin_set') {
-        document.getElementById("main_top").innerHTML = '<h1>Skin settings</h1>';
-        document.title = document.title.replace(/.*(\- .*)$/, "Skin settings $1");
+        set_language = {
+            "en-US" : {
+                "default" : "Default",
+                "change_to_noraml" : "Change to normal text",
+                "delete" : "Delete",
+                "skin_setting" : "Skin settings",
+                "include_link" : "Using include link",
+                "save" : "Save",
+                "strike" : "Strike",
+                "bold" : "Bold",
+                "other" : "Other"
+            }, "ko-KR" : {
+                "default" : "기본값",
+                "change_to_noraml" : "일반 텍스트로 변경",
+                "delete" : "삭제",
+                "skin_setting" : "스킨 설정",
+                "include_link" : "틀 링크 사용",
+                "save" : "저장",
+                "strike" : "취소선",
+                "bold" : "볼드체",
+                "other" : "기타"
+            }
+        }
+
+        language = cookies.match(regex_data('language'))[1];
+        user_language = cookies.match(regex_data('user_language'))[1];
+        if(user_language in set_language) {
+            language = user_language;
+        }
+
+        document.getElementById("main_top").innerHTML = '<h1>' + set_language[language]['skin_setting'] + '</h1>';
+        document.title = document.title.replace(/.*(\- .*)$/, set_language[language]['skin_setting'] + " $1");
         
         data = document.getElementById("main_data");
         set_data = {};
@@ -74,56 +104,56 @@ window.onload = function () {
         if(cookies.match(regex_data('del_strike'))) {
             if(cookies.match(regex_data('del_strike'))[1] === '0') {
                 set_data["strike"] = ' \
-                    <option value="normal">Default</option> \
-                    <option value="change">Change to normal text</option> \
-                    <option value="delete">Delete</option> \
+                    <option value="normal">' + set_language[language]['default'] + '</option> \
+                    <option value="change">' + set_language[language]['change_to_normal'] + '</option> \
+                    <option value="delete">' + set_language[language]['delete'] + '</option> \
                 ';
             } else if(cookies.match(regex_data('del_strike'))[1] === '1') {
                 set_data["strike"] = ' \
-                    <option value="change">Change to normal text</option> \
-                    <option value="normal">Default</option> \
-                    <option value="delete">Delete</option> \
+                    <option value="change">' + set_language[language]['change_to_normal'] + '</option> \
+                    <option value="normal">' + set_language[language]['default'] + '</option> \
+                    <option value="delete">' + set_language[language]['delete'] + '</option> \
                 ';
             } else {
                 set_data["strike"] = ' \
-                    <option value="delete">Delete</option> \
-                    <option value="normal">Default</option> \
-                    <option value="change">Change to normal text</option> \
+                    <option value="delete">' + set_language[language]['delete'] + '</option> \
+                    <option value="normal">' + set_language[language]['default'] + '</option> \
+                    <option value="change">' + set_language[language]['change_to_normal'] + '</option> \
                 ';
             }
         } else {
             set_data["strike"] = ' \
-                <option value="normal">Default</option> \
-                <option value="change">Change to normal text</option> \
-                <option value="delete">Delete</option> \
+                <option value="normal">' + set_language[language]['default'] + '</option> \
+                <option value="change">' + set_language[language]['change_to_normal'] + '</option> \
+                <option value="delete">' + set_language[language]['delete'] + '</option> \
             ';
         }
 
         if(cookies.match(regex_data('del_bold'))) {
             if(cookies.match(regex_data('del_bold'))[1] === '0') {
                 set_data["bold"] = ' \
-                    <option value="normal">Default</option> \
-                    <option value="change">Change to normal text</option> \
-                    <option value="delete">Delete</option> \
+                    <option value="normal">' + set_language[language]['default'] + '</option> \
+                    <option value="change">' + set_language[language]['change_to_normal'] + '</option> \
+                    <option value="delete">' + set_language[language]['delete'] + '</option> \
                 ';
             } else if(cookies.match(regex_data('del_bold'))[1] === '1') {
                 set_data["bold"] = ' \
-                    <option value="change">Change to normal text</option> \
-                    <option value="normal">Default</option> \
-                    <option value="delete">Delete</option> \
+                    <option value="change">' + set_language[language]['change_to_normal'] + '</option> \
+                    <option value="normal">' + set_language[language]['default'] + '</option> \
+                    <option value="delete">' + set_language[language]['delete'] + '</option> \
                 ';
             } else {
                 set_data["bold"] = ' \
-                    <option value="delete">Delete</option> \
-                    <option value="normal">Default</option> \
-                    <option value="change">Change to normal text</option> \
+                    <option value="delete">' + set_language[language]['delete'] + '</option> \
+                    <option value="normal">' + set_language[language]['default'] + '</option> \
+                    <option value="change">' + set_language[language]['change_to_normal'] + '</option> \
                 ';
             }
         } else {
             set_data["bold"] = ' \
-                <option value="normal">Default</option> \
-                <option value="change">Change to normal text</option> \
-                <option value="delete">Delete</option> \
+                <option value="normal">' + set_language[language]['default'] + '</option> \
+                <option value="change">' + set_language[language]['change_to_normal'] + '</option> \
+                <option value="delete">' + set_language[language]['delete'] + '</option> \
             ';
         }
         
@@ -135,19 +165,19 @@ window.onload = function () {
         }
 
         data.innerHTML = ' \
-            <h2>Strike</h2> \
+            <h2>' + set_language[language]['strike'] + '</h2> \
             <hr class="main_hr"> \
             <select id="strike" name="strike"> \
                 ' + set_data["strike"] + ' \
             </select> \
-            <h2>Bold</h2> \
+            <h2>' + set_language[language]['bold'] + '</h2> \
             <select id="bold" name="bold"> \
                 ' + set_data["bold"] + ' \
             </select> \
-            <h2>Other</h2> \
-            <input ' + set_data["include"] + ' type="checkbox" id="include" name="include" value="include"> Using include link \
+            <h2>' + set_language[language]['other'] + '</h2> \
+            <input ' + set_data["include"] + ' type="checkbox" id="include" name="include" value="include"> ' + set_language[language]['include_link'] + ' \
             <hr class="main_hr"> \
-            <button onclick="get_post();">Save</button> \
+            <button onclick="get_post();">' + set_language[language]['save'] + '</button> \
         ';
     }
 }
