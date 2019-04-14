d11ad14b43af2d41c0183a4680c3d6a4f0909904
Surplus_Up (2DU) <min08101@naver.com>
2019-04-14 13:40:19 +0900

    프리뷰 JS로 대체

--- route/api_w.py
+++ route/api_w.py
@@ -3,11 +3,16 @@ from .tool.func import *
 def api_w_2(conn, name):
     curs = conn.cursor()
 
-    curs.execute("select data from data where title = ?", [name])
-    data = curs.fetchall()
-    if data:
-        json_data = { "title" : name, "data" : render_set(title = name, data = data[0][0]) }
-    
+    if flask.request.method == 'POST':
+        json_data = { "title" : name, "data" : render_set(title = name, data = flask.request.form.get('data', '')) }
+        
         return flask.jsonify(json_data)
     else:
-        return flask.jsonify({})
+        curs.execute("select data from data where title = ?", [name])
+        data = curs.fetchall()
+        if data:
+            json_data = { "title" : name, "data" : render_set(title = name, data = data[0][0]) }
+        
+            return flask.jsonify(json_data)
+        else:
+            return flask.jsonify({})
