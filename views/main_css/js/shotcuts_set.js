4ef6c59575ad862311184d4cbba5b3410b64a6af
Surplus_Up (2DU) <min08101@naver.com>
2019-07-08 00:47:28 +0900

    추가 단축키

--- views/main_css/js/shotcuts_set.js
+++ views/main_css/js/shotcuts_set.js
@@ -12,3 +12,20 @@ all_list.forEach(function(element) {
         'disable_in_input' : true
     }); 
 });
+
+all_list_2 = [
+    ['W', '/w'],
+    ['H', '/history'],
+    ['E', '/edit'],
+];
+
+all_list_2.forEach(function(element) {    
+    shortcut.add(element[0], function() {
+        href_d = window.location.href.split("/");
+        if(href_d[4]) { 
+            window.location.href = element[1] + '/' + href_d[4];
+        }
+    }, {
+        'disable_in_input' : true
+    });
+});
