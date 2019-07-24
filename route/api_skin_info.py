f63b15f7c86a5e61e1784ce033ca6f9882a5b3e7
Surplus_Up (2DU) <min08101@naver.com>
2019-07-25 02:23:40 +0900

    skin_info API 기능 확장

--- route/api_skin_info.py
+++ route/api_skin_info.py
@@ -1,15 +1,23 @@
 from .tool.func import *
 
-def api_skin_info_2(conn):
+def api_skin_info_2(conn, name):
     curs = conn.cursor()
 
-    json_address = re.sub("(((?!\.|\/).)+)\.html$", "info.json", skin_check())
+    if name == '':
+        name = skin_check()
+    else:
+        name = './views/' + name + '/index.html'
+
+    print(skin_check())
+    print(name)
+
+    json_address = re.sub("(((?!\.|\/).)+)\.html$", "info.json", name)
     try:
         json_data = json.loads(open(json_address).read())
     except:
         json_data = None
 
-    if json_data:    
+    if json_data:
         return flask.jsonify(json_data)
     else:
         return flask.jsonify({}), 404
