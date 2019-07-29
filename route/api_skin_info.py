ed90fc77d371b2513f5a2ca9a7d4ec376be133e5
Surplus_Up (2DU) <min08101@naver.com>
2019-07-29 22:46:11 +0900

    스킨 버전 표기

--- route/api_skin_info.py
+++ route/api_skin_info.py
@@ -8,16 +8,33 @@ def api_skin_info_2(conn, name):
     else:
         name = './views/' + name + '/index.html'
 
-    print(skin_check())
-    print(name)
+    if not flask.request.args.get('all', None):
+        json_address = re.sub("(((?!\.|\/).)+)\.html$", "info.json", name)
+        try:
+            json_data = json.loads(open(json_address).read())
+        except:
+            json_data = None
 
-    json_address = re.sub("(((?!\.|\/).)+)\.html$", "info.json", name)
-    try:
-        json_data = json.loads(open(json_address).read())
-    except:
-        json_data = None
-
-    if json_data:
-        return flask.jsonify(json_data)
+        if json_data:
+            return flask.jsonify(json_data)
+        else:
+            return flask.jsonify({}), 404
     else:
-        return flask.jsonify({}), 404
+        a_data = {}
+        for i in load_skin(skin_check(1), 1):
+            json_address = re.sub("(((?!\.|\/).)+)\.html$", "info.json", './views/' + i + '/index.html')
+            try:
+                json_data = json.loads(open(json_address).read())
+            except:
+                json_data = None
+
+            if json_data:
+                if i == skin_check(1):
+                    json_data = {**json_data, **{ "main" : "true" }}
+
+                a_data = {**a_data, **{ i : json_data }}
+
+        if a_data == {}:
+            return flask.jsonify({}), 404
+        else:
+            return flask.jsonify(a_data)
