f1d0875b9ae0a8b5c42e1ed8d272487be5eefba9
Surplus_Up (2DU) <min08101@naver.com>
2019-08-24 01:08:54 +0900

    API 버그 수정

--- route/api_w.py
+++ route/api_w.py
@@ -3,16 +3,19 @@ from .tool.func import *
 def api_w_2(conn, name):
     curs = conn.cursor()
 
-    if flask.request.method == 'POST':
-        json_data = { "title" : name, "data" : render_set(title = name, data = flask.request.form.get('data', '')) }
-        
-        return flask.jsonify(json_data)
-    else:
-        curs.execute("select data from data where title = ?", [name])
-        data = curs.fetchall()
-        if data:
-            json_data = { "title" : name, "data" : render_set(title = name, data = data[0][0]) }
-        
+    if acl_check(name, 'render') != 1:
+        if flask.request.method == 'POST':
+            json_data = { "title" : name, "data" : render_set(title = name, data = flask.request.form.get('data', '')) }
+            
             return flask.jsonify(json_data)
         else:
-            return flask.jsonify({})
+            curs.execute("select data from data where title = ?", [name])
+            data = curs.fetchall()
+            if data:
+                json_data = { "title" : name, "data" : render_set(title = name, data = data[0][0]) }
+            
+                return flask.jsonify(json_data)
+            else:
+                return flask.jsonify({})
+    else:
+        return flask.jsonify({})
