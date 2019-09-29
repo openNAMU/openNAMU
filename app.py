87c9393229c83a84915ea7b9f64502662c75bf76
Surplus_Up (2DU) <min08101@naver.com>
2019-09-29 23:50:39 +0900

    아닌 것 같다

--- app.py
+++ app.py
@@ -622,11 +622,12 @@ def main_file(data = None):
 def main_error_404(e):
     return main_error_404_2(conn)
 
-app.secret_key = rep_key
-app.wsgi_app = werkzeug.debug.DebuggedApplication(app.wsgi_app, True)
-app.debug = True
+if __name__ == "__main__":
+    app.secret_key = rep_key
+    app.wsgi_app = werkzeug.debug.DebuggedApplication(app.wsgi_app, True)
+    app.debug = True
 
-http_server = tornado.httpserver.HTTPServer(tornado.wsgi.WSGIContainer(app))
-http_server.listen(server_set['port'], address = server_set['host'])
-
-tornado.ioloop.IOLoop.instance().start()
+    http_server = tornado.httpserver.HTTPServer(tornado.wsgi.WSGIContainer(app))
+    http_server.listen(server_set['port'], address = server_set['host'])
+    
+    tornado.ioloop.IOLoop.instance().start()
