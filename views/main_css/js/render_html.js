0e78d7b1dea52d3a7801e8ecc36fb92a92ae14dc
Surplus_Up (2DU) <min08101@naver.com>
2019-10-07 15:40:19 +0900

    HTML 유튜브 지원 추가

--- views/main_css/js/render_html.js
+++ views/main_css/js/render_html.js
@@ -11,6 +11,20 @@ function render_html(name = '') {
         data = data.replace(patt, '<' + t_data[key] + '>$1</' + t_data[key] + '>');
     }
     
+    src_list = {
+        'www.youtube.com' : '1'
+    }
+    data = data.replace(/&lt;iframe( (?:(?:(?!&gt;).)+))&gt;&lt;\/iframe&gt;/g, function(full, in_data) {
+        src_data = data.match(/ src=['"]https:\/\/([^/'"]+)(?:[^'"]+)['"](?: |$)/);
+        if(src_data) {
+            if(src_list[src_data[1]]) {
+                return '<iframe' + in_data + '></iframe>';
+            } else {
+                return full;
+            }
+        }
+    });
+    
     if(name === '') {
         document.getElementById('render_contect').innerHTML = data;
     } else {
