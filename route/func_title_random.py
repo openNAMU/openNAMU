f10d51f93c7b4dee2b2952cac3a7e65a16e30c89
Surplus_Up (2DU) <min08101@naver.com>
2019-10-09 16:52:06 +0900

    mysql에서도 쓸 수 있게

--- route/func_title_random.py
+++ route/func_title_random.py
@@ -3,7 +3,11 @@ from .tool.func import *
 def func_title_random_2(conn):
     curs = conn.cursor()
 
-    curs.execute("select title from data order by random() limit 1")
+    curs.execute("" + \
+        "select title from data " + \
+        "where title not like 'user:%' and title not like 'category:%' and title not like 'file:%'" + \
+        "order by random() limit 1" + \
+    "")
     data = curs.fetchall()
     if data:
         return redirect('/w/' + url_pas(data[0][0]))
