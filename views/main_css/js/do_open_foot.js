5cdf280d17c3e51e06699de583cc03449d6a8270
Surplus_Up (2DU) <min08101@naver.com>
2019-10-12 23:31:38 +0900

    사소한 수정 4

--- views/main_css/js/do_open_foot.js
+++ views/main_css/js/do_open_foot.js
@@ -2,13 +2,14 @@ function do_open_foot(name, num = 0) {
     var found_include = name.match(/^(include_(?:[0-9]+)\-)/);
     if(found_include) {
         var include_name = name.replace(/^(?:include_(?:[0-9]+)\-)/, '');
+        document.getElementById(found_include[1] + 'r' + include_name).style.color = 'red';
         if(num === 1) {
             document.getElementById(found_include[1] + 'c' + include_name).style.color = 'inherit';
         } else {
             document.getElementById(found_include[1] + 'c' + include_name).style.color = 'red';
         }
     } else {
-        var d_color = document.getElementById('c' + name).style.color;
+        document.getElementById('r' + name).style.color = 'red';
         if(num === 1) {
             document.getElementById('c' + name).style.color = 'inherit';
         } else {
