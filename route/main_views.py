7c50e2bab98c123d862eb1005cf3ff33fb915946
Surplus_Up (2DU) <min08101@naver.com>
2019-10-20 18:17:49 +0900

    필요 없는 코드 정리

--- route/main_views.py
+++ route/main_views.py
@@ -2,40 +2,18 @@ from .tool.func import *
 
 def main_views_2(conn, name):
     curs = conn.cursor()
-
-    m = re.search('\.([^.]+)$', name)
-    if m:
-        g = m.groups()
-    else:
-        g = ['']
-
-    if g[0] == 'css':
-        c_open = open('./views/' + name, encoding='utf-8')
-        f_open = c_open.read()
-        c_open.close()
-        return flask.Response(easy_minify(f_open, 'css'), mimetype = "text/css")
-    elif g[0] == 'js':
-        c_open = open('./views/' + name, encoding='utf-8')
-        f_open = c_open.read()
-        c_open.close()
-        return flask.Response(easy_minify(f_open, 'js'), mimetype = "text/js")
-    elif g[0] == 'html':
-        c_open = open('./views/' + name, encoding='utf-8')
-        f_open = c_open.read()
-        c_open.close()
-        return flask.Response(easy_minify(f_open), mimetype = "text/html")
-    else:
-        if re.search('\/', name):
-            m = re.search('^(.*)\/(.*)$', name)
-            if m:
-                n = m.groups()
-                plus = '/' + n[0]
-                rename = n[1]
-            else:
-                plus = ''
-                rename = name
+    
+    if re.search('\/', name):
+        m = re.search('^(.*)\/(.*)$', name)
+        if m:
+            n = m.groups()
+            plus = '/' + n[0]
+            rename = n[1]
         else:
             plus = ''
             rename = name
+    else:
+        plus = ''
+        rename = name
 
-        return flask.send_from_directory('./views' + plus, rename)
+    return flask.send_from_directory('./views' + plus, rename)
