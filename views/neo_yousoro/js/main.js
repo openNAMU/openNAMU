60aa14a2b01dee9d7aae75eb4b7d4a95445d9896
Surplus_Up (2DU) <min08101@naver.com>
2019-10-27 21:58:55 +0900

    스킨 이름 변경

--- views/neo_yousoro/js/main.js
+++
@@ -1,8 +0,0 @@
-function opening(data) {
-    var element = document.getElementById(data);
-    if(element.style.display == 'none') {
-        element.style.display = 'block';
-    } else {
-        element.style.display = 'none';
-    }
-}
