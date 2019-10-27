8bf1db2f658fad9c8ee7984f89efb24131e1a0a9
Surplus_Up (2DU) <min08101@naver.com>
2019-10-27 17:13:55 +0900

    스킨 검색 미리보기 Beta

---
+++ views/neo_yousoro/js/search.js
@@ -0,0 +1,34 @@
+window.onload = function () {
+    var before = '';
+    setInterval(function() {
+        var data = document.getElementById("search_input").value;
+        if(before !== data && data !== '') {
+            before = data;
+            var url = "/api/search/" + encodeURI(data) + "?num=10";
+        
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", url, true);
+            xhr.send(null);
+            
+            xhr.onreadystatechange = function() {
+                if(this.readyState === 4 && this.status === 200) {
+                    document.getElementById("pre_search").style.display = 'block';
+
+                    var get_data = JSON.parse(this.responseText);
+                    document.getElementById("pre_search").innerHTML = '';
+
+                    if(this.responseText !== "{}\n") {
+                        for(key in get_data) {
+                            document.getElementById("pre_search").innerHTML += '<a href="/w/' + encodeURI(get_data[key][0]) + '">' + get_data[key][0] + '</a><br>';
+                        }
+                    } else {
+                        document.getElementById("pre_search").style.display = 'none';
+                    }
+                }
+            }
+        } else if(before !== data && data === '') {
+            before = '';
+            document.getElementById("pre_search").style.display = 'none';
+        }
+    }, 1000);
+}
