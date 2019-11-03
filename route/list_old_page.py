b0d4038105aacb69a298b3864d122e9732e23437
Surplus_Up (2DU) <min08101@naver.com>
2019-11-03 21:07:43 +0900

    버그 수정

--- route/list_old_page.py
+++ route/list_old_page.py
@@ -14,13 +14,14 @@ def list_old_page_2(conn):
     curs.execute('' + \
         'select title, date from history ' + \
         "where title not like 'user:%' and title not like 'category:%' and title not like 'file:%' " + \
-        'order by id desc, date asc ' + \
+        'group by title '
+        'order by date asc ' + \
         'limit ?, "50"' + \
     '', [str(sql_num)])
     n_list = curs.fetchall()
     for data in n_list:
         div += '<li><a href="/w/' + url_pas(data[0]) + '">' + html.escape(data[0]) + '</a> (' + re.sub(' .*$', '', data[1]) + ')</li>'
-            
+    
     div += '</ul>' + next_fix('/old_page?num=', num, n_list)
 
     return easy_minify(flask.render_template(skin_check(), 
