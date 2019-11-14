a6b2d97be6048a54e5297ac3f94ae13b2b3f6696
Surplus_Up (2DU) <min08101@naver.com>
2019-11-15 02:04:35 +0900

    토론에도 미리보기 추가

--- route/edit.py
+++ route/edit.py
@@ -117,7 +117,7 @@ def edit_2(conn, name):
                 <form method="post">
                     <script>do_stop_exit()</script>
                     ''' + edit_button() + '''
-                    <textarea id="content" rows="25" id="content" name="content">''' + html.escape(re.sub('\n$', '', data)) + '''</textarea>
+                    <textarea rows="25" id="content" name="content">''' + html.escape(re.sub('\n$', '', data)) + '''</textarea>
                     <textarea style="display: none;" name="otent">''' + html.escape(re.sub('\n$', '', data_old)) + '''</textarea>
                     <hr class=\"main_hr\">
                     <input placeholder="''' + load_lang('why') + '''" name="send" type="text">
