16e4d10c9897ae282940206b234642bc43c92505
Surplus_Up (2DU) <min08101@naver.com>
2019-11-15 23:28:46 +0900

    저장할 땐 안 뜨게

--- views/main_css/js/do_stop_exit.js
+++ views/main_css/js/do_stop_exit.js
@@ -1,3 +1,12 @@
+var go_save_zone = 0;
+
 function do_stop_exit() {
-    window.onbeforeunload = function(){ return ''; }
+    window.onbeforeunload = function() {
+        console.log(go_save_zone);
+        data = document.getElementById('content').value;
+        origin = document.getElementById('origin').value;
+        if(data !== origin && go_save_zone != 1) {
+            return ''; 
+        }
+    }
 }
