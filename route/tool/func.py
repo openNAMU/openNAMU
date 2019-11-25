58955106838e0f39cb3768678d71851c971dd773
잉여개발기 (SPDV) <min08101@naver.com>
2019-11-25 21:23:14 +0900

    잘못 고쳐서 다시 고침

--- route/tool/func.py
+++ route/tool/func.py
@@ -402,7 +402,8 @@ def other2(data):
         'topic_main_load.js' : '2',
         'topic_plus_load.js' : '2',
         'do_stop_exit.js' : '2',
-        'do_open_folding' : '2'
+        'do_open_folding.js' : '2',
+        'shotcuts_set.js' : '2'
     }
     for i_data in os.listdir(os.path.join("views", "main_css", "js")):
         if i_data in js_filter:
