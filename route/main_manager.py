53ebba2a8bc82122b682dcacb32ba280ca380b61
Surplus_Up (2DU) <min08101@naver.com>
2019-12-02 23:46:47 +0900

    자동 인덱스 되도록 수정

--- route/main_manager.py
+++ route/main_manager.py
@@ -1,6 +1,6 @@
 from .tool.func import *
 
-def main_manager_2(conn, num, r_ver, db_type):
+def main_manager_2(conn, num, r_ver):
     curs = conn.cursor()
     
     title_list = {
@@ -51,7 +51,6 @@ def main_manager_2(conn, num, r_ver, db_type):
                     <br>
                     <h2>''' + load_lang('server') + '''</h2>
                     <ul>
-                        ''' + (('<li><a href="/indexing">' + load_lang('indexing') + '</a></li>') if db_type == 'sqlite' else '') + '''
                         <li><a href="/restart">''' + load_lang('wiki_restart') + '''</a></li>
                         <li><a href="/update">''' + load_lang('update') + '''</a></li>
                         <li><a href="/oauth_setting">''' + load_lang('oauth_setting') + '''</a></li>
