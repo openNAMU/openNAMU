202b962a0b3b58c7f2b297e116cbbdeb6ac1f4d3
Surplus_Up (2DU) <min08101@naver.com>
2019-12-05 00:39:55 +0900

    파일 정리

--- route/main_file.py
+++ route/main_file.py
@@ -4,8 +4,13 @@ from . import main_error_404
 def main_file_2(conn, data):
     curs = conn.cursor()
 
-    print(data)
-    if re.search('\.txt$', data) or data == 'sitemap.xml':
+    if data == 'easter_egg.html':
+        return easy_minify(flask.render_template(skin_check(), 
+            imp = ['easter_egg.html', wiki_set(), custom(), other2([0, 0])],
+            data = open('./views/main_css/file/easter_egg.html', 'r').read(),
+            menu = 0
+        ))
+    elif re.search('\.txt$', data) or data == 'sitemap.xml':
         if data == 'robots.txt' and not os.path.exists('robots.txt'):
             return flask.Response('User-agent: *\nDisallow: /\nAllow: /$\nAllow: /w/', mimetype='text/plain')
         elif os.path.exists(data):
