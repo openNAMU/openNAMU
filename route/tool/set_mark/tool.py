4a303533427461e70163f079558f0568944cbfda
잉여개발기 (SPDV) <min08101@naver.com>
2019-12-08 02:16:51 +0900

    또 버그 수정

--- route/tool/set_mark/tool.py
+++ route/tool/set_mark/tool.py
@@ -51,11 +51,11 @@ def link_fix(main_link):
     main_link = re.sub('^파일:', 'file:', main_link)
     main_link = re.sub('^분류:', 'category:', main_link)
 
-    other_link = re.search('[^\\\\]?(#.+)$', main_link)
+    other_link = re.search('[^\\\\]?(#[^#]+)$', main_link)
     if other_link:
         other_link = other_link.groups()[0]
 
-        main_link = re.sub('(#.+)$', '', main_link)
+        main_link = re.sub('(#[^#]+)$', '', main_link)
     else:
         other_link = ''
 
