9e63e41c51c8b3f0f906a74e73431fe6ac897742
잉여개발기 (SPDV) <min08101@naver.com>
2019-12-08 01:20:07 +0900

    빼 먹은 거 추가

--- route/tool/func.py
+++ route/tool/func.py
@@ -398,7 +398,7 @@ def other2(data):
     req_list = ''
     
     css_filter = {
-        'main.css' : '6'
+        'main.css' : '7'
     }
     for i_data in os.listdir(os.path.join("views", "main_css", "css")):
         if i_data in css_filter:
