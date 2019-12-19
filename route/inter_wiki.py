d1ab244071b3d83e35086b45bfe1bd8eaf45643a
Surplus_Up (2DU) <min08101@naver.com>
2019-12-19 21:18:47 +0900

    한메일 빼고 카카오메일 추가

--- route/inter_wiki.py
+++ route/inter_wiki.py
@@ -17,15 +17,14 @@ def inter_wiki_2(conn, tools):
         del_link = 'del_email_filter'
         plus_link = 'plus_email_filter'
         title = load_lang('email_filter_list')
-        div =   '''
-                <ul>
-                    <li>gmail.com</li>
-                    <li>naver.com</li>
-                    <li>daum.net</li>
-                    <li>hanmail.net</li>
-                    <li>hanmail2.net</li>
-                </ul>
-                '''
+        div = '''
+            <ul>
+                <li>gmail.com</li>
+                <li>naver.com</li>
+                <li>daum.net</li>
+                <li>kakao.com</li>
+            </ul>
+        '''
 
         curs.execute(db_change("select html from html_filter where kind = 'email'"))
     elif tools == 'name_filter':
