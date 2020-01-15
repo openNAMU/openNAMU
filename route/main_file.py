5cc599612c923fe18d759b3b88599c3019c1f6d5
Surplus_Up (2DU) <min08101@naver.com>
2020-01-15 19:38:22 +0900

    일부 마인 타입 더 지정

--- route/main_file.py
+++ route/main_file.py
@@ -10,11 +10,14 @@ def main_file_2(conn, data):
             data = open('./views/main_css/file/easter_egg.html', 'r').read(),
             menu = 0
         ))
-    elif re.search('\.txt$', data) or data == 'sitemap.xml':
+    elif re.search('\.txt$', data, flags = re.I) or data == 'sitemap.xml':
         if data == 'robots.txt' and not os.path.exists('robots.txt'):
-            return flask.Response('User-agent: *\nDisallow: /\nAllow: /$\nAllow: /w/', mimetype='text/plain')
+            return flask.Response('User-agent: *\nDisallow: /\nAllow: /$\nAllow: /w/', mimetype = 'text/plain')
         elif os.path.exists(data):
-            return flask.send_from_directory('./', data)
+            if re.search('\.txt$', data, flags = re.I):
+                return flask.send_from_directory('./', data, mimetype = 'text/plain')
+            else:
+                return flask.send_from_directory('./', data, mimetype = 'text/xml')
         else:
             return main_error_404.main_error_404_2(conn)
     else:
