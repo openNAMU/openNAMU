52efa4c53f9d428ca25442c04861d2e7866d7377
Surplus_Up (2DU) <min08101@naver.com>
2020-01-15 19:38:22 +0900

    일부 마인 타입 더 지정

--- route/main_image_view.py
+++ route/main_image_view.py
@@ -4,6 +4,9 @@ def main_image_view_2(conn, name, app_var):
     curs = conn.cursor()
 
     if os.path.exists(os.path.join(app_var['path_data_image'], name)):
-        return flask.send_from_directory('./' + app_var['path_data_image'], name)
+        return flask.send_from_directory(
+            './' + app_var['path_data_image'], name, 
+            mimetype = 'image/' + re.search('\.([^\.]+)$', name).groups()[0]
+        )
     else:
         return redirect()
--- route/main_views.py
+++ route/main_views.py
@@ -18,7 +18,10 @@ def main_views_2(conn, name):
 
     mime_type = re.search('\.([^\.]+)$', rename).groups()[0]
     if mime_type:
-        mime_type = 'text/' + mime_type
+        if mime_type in ['.jpeg', '.jpg', '.gif', '.png', '.webp', '.JPEG', '.JPG', '.GIF', '.PNG', '.WEBP']:
+            mime_type = 'image/' + mime_type
+        else:
+            mime_type = 'text/' + mime_type
     else:
         mime_type = 'text/plain'
 
