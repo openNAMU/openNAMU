5d17ab58abbdc0ea77b3a971078ae45f994a993e
Surplus_Up (2DU) <min08101@naver.com>
2020-02-10 15:06:29 +0900

    링크 버그 수정, JS 통폐합

--- views/main_css/js/load_namumark.js
+++ views/main_css/js/load_namumark.js
@@ -3,7 +3,7 @@ function get_link_state(data, i = 0) {
         var link_data = document.getElementsByClassName(data + 'link_finder')[i];
 
         var xhr = new XMLHttpRequest();
-        xhr.open("GET", link_data.href.replace('/w/', '/api/w/') + "?exist=1", true);
+        xhr.open("GET", link_data.href.replace('/w/', '/api/w/').replace(/#([^#]*)/, '') + "?exist=1", true);
         xhr.send(null);
 
         xhr.onreadystatechange = function() {
@@ -38,6 +38,36 @@ function get_file_state(data, i = 0) {
             
                 get_file_state(data, i + 1);
             }
+        }
+    }
+}
+
+function load_include(title, name, p_data) {
+    var o_data = document.getElementById(name);
+
+    var change = '';
+    for(key in p_data) {
+        change += '@' + p_data[key][0].replace('&', '<amp>') + '@,' + p_data[key][1].replace(',', '<comma>').replace('&', '<amp>') + ','
+    }
+    
+    var url = "/api/w/" + encodeURI(title) + "?include=" + name + "&change=" + change;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.send(null);
+
+    xhr.onreadystatechange = function() {
+        if(this.readyState === 4 && this.status === 200) {
+            var o_p_data = JSON.parse(this.responseText);
+            var g_data = o_p_data['data'];
+
+            o_data.innerHTML = g_data;
+
+            js_data = o_p_data['js_data'];
+            js_data = js_data.replace(/<script>/g, '');
+            js_data = js_data.replace(/<\/script>/g, '\n');
+
+            eval(js_data)
         }
     }
 }
