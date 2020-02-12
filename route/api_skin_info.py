c8308a6253032273d7f20d42be3f5a67d9a60cc2
Surplus_Up (2DU) <min08101@naver.com>
2020-02-12 23:49:23 +0900

    스킨 최신 버전 확인 기능 추가

--- route/api_skin_info.py
+++ route/api_skin_info.py
@@ -32,6 +32,18 @@ def api_skin_info_2(conn, name):
                 if i == skin_check(1):
                     json_data = {**json_data, **{ "main" : "true" }}
 
+                if "info_link" in json_data:
+                    get_data = urllib.request.urlopen(json_data["info_link"])
+                    if get_data and get_data.getcode() == 200:
+                        try:
+                            get_data = json.loads(get_data.read().decode())
+                            if "skin_ver" in get_data:
+                                json_data = {**json_data, **{ "lastest_version" : {
+                                    "skin_ver" : json_data["skin_ver"]
+                                }}}
+                        except:
+                            pass
+
                 a_data = {**a_data, **{ i : json_data }}
 
         if a_data == {}:
