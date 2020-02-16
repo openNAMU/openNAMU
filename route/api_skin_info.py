a9dfbb251e4f6f47389b794243e29f1088d7847d
Surplus_Up (2DU) <min08101@naver.com>
2020-02-16 22:35:30 +0900

    디폴트 링크 존재하도록

--- route/api_skin_info.py
+++ route/api_skin_info.py
@@ -21,6 +21,12 @@ def api_skin_info_2(conn, name):
             return flask.jsonify({}), 404
     else:
         a_data = {}
+        d_link_data = {
+            "ACME" : "https://raw.githubusercontent.com/openNAMU/openNAMU-Skin-ACME/master/info.json",
+            "Liberty" : "https://raw.githubusercontent.com/openNAMU/openNAMU-Skin-Liberty/master/info.json",
+            "Before Namu" : "https://raw.githubusercontent.com/openNAMU/openNAMU-Skin-Before_Namu/master/info.json"
+        }
+
         for i in load_skin(skin_check(1), 1):
             json_address = re.sub("(((?!\.|\/).)+)\.html$", "info.json", './views/' + i + '/index.html')
             try:
@@ -33,7 +39,16 @@ def api_skin_info_2(conn, name):
                     json_data = {**json_data, **{ "main" : "true" }}
 
                 if "info_link" in json_data:
-                    get_data = urllib.request.urlopen(json_data["info_link"])
+                    info_link = json_data["info_link"]
+                    get_num = 1
+                elif json_data["name"] in d_link_data:
+                    info_link = d_link_data[json_data["name"]]
+                    get_num = 1
+                else:
+                    get_num = 0
+
+                if get_num == 1:
+                    get_data = urllib.request.urlopen(info_link)
                     if get_data and get_data.getcode() == 200:
                         try:
                             get_data = json.loads(get_data.read().decode())
@@ -46,7 +61,4 @@ def api_skin_info_2(conn, name):
 
                 a_data = {**a_data, **{ i : json_data }}
 
-        if a_data == {}:
-            return flask.jsonify({})
-        else:
-            return flask.jsonify(a_data)
+        return flask.jsonify(a_data)
