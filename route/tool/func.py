d53d4f19d4bad98ca020624344d40e08e746c3b0
Surplus_Up (2DU) <min08101@naver.com>
2020-02-18 14:44:44 +0900

    CC 0를 ARR로 수정

--- route/tool/func.py
+++ route/tool/func.py
@@ -530,7 +530,7 @@ def wiki_set(num = 1):
         if db_data and db_data[0][0] != '':
             data_list += [db_data[0][0]]
         else:
-            data_list += ['CC 0']
+            data_list += ['ARR']
 
         data_list += ['', '']
 
