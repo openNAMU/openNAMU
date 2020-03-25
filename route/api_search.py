d5db169567fea5e660a53a2629c667a96fa99f77
Surplus_Up (2DU) <min08101@naver.com>
2020-03-26 02:31:13 +0900

    버그 수정 및 검색 속도 향상(?)

--- route/api_search.py
+++ route/api_search.py
@@ -19,14 +19,15 @@ def api_search_2(conn, name):
     curs.execute(db_change('select data from other where name = "count_all_title"'))
     if int(curs.fetchall()[0][0]) < 30000:
         curs.execute(db_change("" + \
-            "select distinct title, case when title like ? then 'title' else 'data' " + \
-            "end from data where title like ? or data like ? order by case " + \
-            "when title like ? then 1 else 2 end limit ?, ?"),
+            "select distinct title, case " + \
+            "when title >= '' and title like ? then 'title' else 'data' end from data " + \
+            "where title >= '' and (title like ? or data like ?) order by case " + \
+            "when title >= '' and title like ? then 1 else 2 end limit ?, ?"),
             ['%' + name + '%', '%' + name + '%', '%' + name + '%', '%' + name + '%', page, num]
         )
     else:
-        curs.execute(db_change("select title from data where title like ? order by title limit ?, 50"),
-            ['%' + name + '%', sql_num]
+        curs.execute(db_change("select title from data where title >= '' and title like ? order by title limit ?, ?"),
+            ['%' + name + '%', page, num]
         )
     all_list = curs.fetchall()
     if all_list:
