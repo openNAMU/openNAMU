8f66a0b362f9708750601ab9866a6366e479a8e7
Surplus_Up (2DU) <min08101@naver.com>
2020-03-25 19:22:59 +0900

    토론 기록 수정, 구형 코드 제거

--- route/list_not_close_topic.py
+++ route/list_not_close_topic.py
@@ -5,11 +5,10 @@ def list_not_close_topic_2(conn):
 
     div = '<ul>'
 
-    curs.execute(db_change('select title, sub, date from rd where stop != "O" order by date desc'))
+    curs.execute(db_change('select title, sub, date, code from rd where stop != "O" order by date desc'))
     n_list = curs.fetchall()
     for data in n_list:
-        curs.execute(db_change("select code from topic where id = '1' and title = ? and sub = ?"), [data[0], data[1]])
-        div += '<li><a href="/thread/' + url_pas(curs.fetchall()[0][0]) + '">' + html.escape(data[0]) + '</a> (' + data[1] + ') | ' + data[2] + '</li>'
+        div += '<li>' + data[2] + ' : <a href="/thread/' + data[3] + '">' + html.escape(data[1]) + '</a> (' + html.escape(data[0]) + ')</li>'
 
     div += '</ul>'
 
--- route/list_user_topic.py
+++ route/list_user_topic.py
@@ -9,46 +9,31 @@ def list_user_topic_2(conn, name):
     else:
         sql_num = 0
 
-    one_admin = admin_check(1)
+    div = '''
+        <table id="main_table_set">
+            <tbody>
+                <tr>
+                    <td id="main_table_width">''' + load_lang('discussion_name') + '''</td>
+                    <td id="main_table_width">''' + load_lang('writer') + '''</td>
+                    <td id="main_table_width">''' + load_lang('time') + '''</td>
+                </tr>
+    '''
 
-    div =   '''
-            <table id="main_table_set">
-                <tbody>
-                    <tr>
-                        <td id="main_table_width">''' + load_lang('discussion_name') + '''</td>
-                        <td id="main_table_width">''' + load_lang('writer') + '''</td>
-                        <td id="main_table_width">''' + load_lang('time') + '''</td>
-                    </tr>
-            '''
-
-    curs.execute(db_change("select title, id, sub, ip, date from topic where ip = ? order by date desc limit ?, 50"), [name, sql_num])
+    curs.execute(db_change("select code, id, ip, date from topic where ip = ? order by date desc limit ?, 50"), [name, sql_num])
     data_list = curs.fetchall()
     for data in data_list:
         title = html.escape(data[0])
         sub = html.escape(data[2])
 
-        if one_admin == 1:
-            curs.execute(db_change("select * from ban where block = ?"), [data[3]])
-            if curs.fetchall():
-                ban = ' <a href="/ban/' + url_pas(data[3]) + '">(' + load_lang('release') + ')</a>'
-            else:
-                ban = ' <a href="/ban/' + url_pas(data[3]) + '">(' + load_lang('ban') + ')</a>'
-        else:
-            ban = ''
-
-        curs.execute(db_change("select code from topic where id = '1' and title = ? and sub = ?"), [title, sub])
-        div += '<tr><td><a href="/thread/' + url_pas(curs.fetchall()[0][0]) + '#' + data[1] + '">' + title + '#' + data[1] + '</a> (' + sub + ')</td>'
-        div += '<td>' + ip_pas(data[3]) + ban + '</td><td>' + data[4] + '</td></tr>'
+        curs.execute(db_change("select title, sub from rd where code = ?"), [data[0]])
+        other_data = curs.fetchall()
+        
+        div += '<tr><td><a href="/thread/' + data[0] + '#' + data[1] + '">' + other_data[0][1] + '#' + data[1] + '</a> (' + other_data[0][0] + ')</td>'
+        div += '<td>' + ip_pas(data[2]) + '</td><td>' + data[3] + '</td></tr>'
 
     div += '</tbody></table>'
     div += next_fix('/topic_record/' + url_pas(name) + '?num=', num, data_list)
-
-    curs.execute(db_change("select end from ban where block = ?"), [name])
-    if curs.fetchall():
-        sub = ' (' + load_lang('blocked') + ')'
-    else:
-        sub = 0
-
+    
     return easy_minify(flask.render_template(skin_check(),
         imp = [load_lang('discussion_record'), wiki_set(), custom(), other2([sub, 0])],
         data = div,
