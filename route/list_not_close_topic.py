4568b048262b1d9c05f9e5a5d94b9516f262e8eb
Surplus_Up (2DU) <min08101@naver.com>
2020-03-29 01:49:22 +0900

    ACL 문서 목록 개편

--- route/list_not_close_topic.py
+++ route/list_not_close_topic.py
@@ -8,7 +8,7 @@ def list_not_close_topic_2(conn):
     curs.execute(db_change('select title, sub, date, code from rd where stop != "O" order by date desc'))
     n_list = curs.fetchall()
     for data in n_list:
-        div += '<li>' + data[2] + ' : <a href="/thread/' + data[3] + '">' + html.escape(data[1]) + '</a> (' + html.escape(data[0]) + ')</li>'
+        div += '<li>' + data[2] + ' | <a href="/thread/' + data[3] + '">' + html.escape(data[1]) + '</a> (' + html.escape(data[0]) + ')</li>'
 
     div += '</ul>'
 
