ee57b2bef8e552ba6e5e0b595cdb73f68aad89ab
gdl-888 <55873929+gdl-888@users.noreply.github.com>
2020-03-30 05:24:45 -0700

    오류 수정-2

--- route/login_logout.py
+++ route/login_logout.py
@@ -2,6 +2,8 @@ from .tool.func import *
 
 def login_logout_2(conn):
     curs = conn.cursor()
+    
+    resp = flask.make_response(redirect('/user'))
 
     flask.session.pop('state', None)
     flask.session.pop('id', None)
@@ -9,4 +11,4 @@ def login_logout_2(conn):
     resp.set_cookie('autologin_token', '', expires=0)
     resp.set_cookie('autologin_username', '', expires=0)
 
-    return redirect('/user')
+    return resp
