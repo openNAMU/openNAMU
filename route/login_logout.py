ace878d3ca9ae104390cc99576ba46dd6e6e4b53
gdl-888 <55873929+gdl-888@users.noreply.github.com>
2020-03-30 03:29:46 -0700

    ㅗ안

--- route/login_logout.py
+++ route/login_logout.py
@@ -6,4 +6,7 @@ def login_logout_2(conn):
     flask.session.pop('state', None)
     flask.session.pop('id', None)
 
+    resp.set_cookie('autologin_token', '', expires=0)
+    resp.set_cookie('autologin_username', '', expires=0)
+
     return redirect('/user')
