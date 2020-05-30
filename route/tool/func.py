c64b290dadeb8b0a63c72e1161f2769f796a4b10
Surplus_Up (2DU) <min08101@naver.com>
2020-05-30 18:27:16 +0900

    init 잘못된 부분 수정

--- route/tool/func.py
+++ route/tool/func.py
@@ -257,7 +257,7 @@ def set_init():
 
     curs.execute(db_change('select data from other where name = "smtp_server" or name = "smtp_port" or name = "smtp_security"'))
     if not curs.fetchall():
-        for i in [['smtp_server', 'imap.google.com'], ['smtp_port', '587'], ['smtp_security', 'tls']]:
+        for i in [['smtp_server', 'smtp.gmail.com'], ['smtp_port', '587'], ['smtp_security', 'tls']]:
             curs.execute(db_change("insert into other (name, data) values (?, ?)"), [i[0], i[1]])
 
 def pw_encode(data, data2 = '', type_d = ''):
