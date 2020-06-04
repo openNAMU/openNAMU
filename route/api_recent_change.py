d757b03ddeed158330b63955b3ee03651b8aed53
Surplus_Up (2DU) <min08101@naver.com>
2020-06-04 19:41:38 +0900

    일부 롤백, 일부 코드 수정

--- route/api_recent_change.py
+++ route/api_recent_change.py
@@ -4,24 +4,14 @@ def api_recent_change_2(conn):
     curs = conn.cursor()
 
     num = int(number_check(flask.request.args.get('num', '10')))
-    if not num > 0:
-        num = 1
-
-    if num > 1000:
-        num = 1
-
-    page = int(number_check(flask.request.args.get('page', '1')))
-    if page * num > 0:
-        page = page * num - num
-    else:
-        page = 0
+    num = 50 if (1 if not num > 0 else num) > 50 else num
 
     curs.execute(db_change('' + \
         'select id, title, date, ip, send, leng from history ' + \
         "where not title like 'user:%' " + \
         'order by date desc ' + \
-        'limit ?, ?' + \
-    ''), [page, num])
+        'limit ?' + \
+    ''), [num])
     all_list = curs.fetchall()
     if all_list:
         return flask.jsonify(all_list)
