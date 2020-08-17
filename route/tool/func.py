2c7b01e205d0a5d5632619a270991d9e591acc7c
잉여개발기 (SPDV) <min08101@naver.com>
2020-08-17 22:17:48 +0900

    디자인 수정

--- route/tool/func.py
+++ route/tool/func.py
@@ -515,7 +515,7 @@ def next_fix(link, num, page, end = 50):
 
 def other2(data):
     global req_list
-    main_css_ver = '51'
+    main_css_ver = '52'
     data += ['' for _ in range(0, 3 - len(data))]
 
     if req_list == '':
