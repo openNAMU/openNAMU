7d054611b5f636e7ff4d7b7db1cb3334f276b94e
잉여개발기 (SPDV) <min08101@naver.com>
2020-08-24 19:08:18 +0900

    아이피 로드 방식 좀 변경

--- route/tool/set_mark/tool.py
+++ route/tool/set_mark/tool.py
@@ -23,19 +23,17 @@ def ip_check(d_type = 0):
     ip = ''
     if d_type == 0 and (flask.session and 'id' in flask.session):
         ip = flask.session['id']
-    
-    if ip == '':
-        try:
-            ip = flask.request.environ.get('HTTP_X_REAL_IP', flask.request.environ.get('HTTP_X_FORWARDED_FOR', flask.request.remote_addr))
-            ip = ip[0] if type(ip) == type([]) else ip
+    else:
+        ip_list = [
+            flask.request.environ.get('HTTP_X_REAL_IP', '::1'),
+            flask.request.environ.get('HTTP_X_FORWARDED_FOR', '::1'),
+            flask.request.environ.get('REMOTE_ADDR', '::1')
+        ]
+        for ip in ip_list:
+            if not (ip == '::1' or ip == '127.0.0.1'):
+                ip = ip[0] if type(ip) == type([]) else ip.split(',')[0]
 
-            if ip == '::1' or ip == '127.0.0.1':
-                ip = flask.request.environ.get('HTTP_X_FORWARDED_FOR', flask.request.remote_addr)
-                ip = ip[0] if type(ip) == type([]) else ip
-        except:
-            ip = 'error:ip'
-
-    return str(ip)
+    return ip
 
 def url_pas(data):
     return urllib.parse.quote(data).replace('/','%2F')
