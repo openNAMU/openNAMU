32e2d9a4f8724db8d9fb7301d5a0f21fc8b7f71f
Surplus_Up (2DU) <min08101@naver.com>
2020-11-14 00:42:36 +0900

    일부 CSS 변경

--- route/tool/func.py
+++ route/tool/func.py
@@ -561,7 +561,7 @@ def next_fix(link, num, page, end = 50):
 
 def other2(data):
     global req_list
-    main_css_ver = '57'
+    main_css_ver = '58'
     data += ['' for _ in range(0, 3 - len(data))]
 
     if req_list == '':
