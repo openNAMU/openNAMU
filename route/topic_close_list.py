e6c749e29a041ee516ddcc5dd3daf0bea64c509d
2du <min08101@naver.com>
2021-03-24 22:33:15 +0900

    topic sys remake & ie eos

--- route/topic_close_list.py
+++ route/topic_close_list.py
@@ -35,39 +35,35 @@ def topic_close_list_2(conn, name):
 
         plus = '''
             <a href="/topic/''' + url_pas(name) + '?tool=close">(' + load_lang('closed_discussion') + ')</a> <a href="/topic/' + url_pas(name) + '?tool=agree">(' + load_lang('agreed_discussion') + ''')</a>
-            <hr class=\"main_hr\">
+            <hr class="main_hr">
             <form style="''' + display + '" method="post" action="/thread/' + topic_num + '''">
                 <input placeholder="''' + load_lang('discussion_name') + '''" name="title">
-                <hr class=\"main_hr\">
+                <hr class="main_hr">
                 <textarea rows="10" id="content" placeholder="''' + load_lang('content') + '''" name="content"></textarea>
-                <hr class=\"main_hr\">
+                <hr class="main_hr">
                 ''' + captcha_get() + (ip_warring() if display == '' else '') + '''
                 <input style="display: none;" name="topic" value="''' + name + '''">
                 <button type="submit">''' + load_lang('send') + '''</button>
                 <button id="preview" type="button" onclick="load_preview(\'''' + url_pas(name) + '\')">' + load_lang('preview') + '''</button>
             </form>
-            <hr class=\"main_hr\">
+            <hr class="main_hr">
             <div id="see_preview"></div>
         '''
 
         curs.execute(db_change("select code, sub from rd where title = ? and stop != 'O' order by date desc"), [name])
 
-    t_num = 0
     for data in curs.fetchall():
-        t_num += 1
-        t_str_num = str(t_num)
-
-        curs.execute(db_change("select id from topic where code = ? order by date desc limit 1"), [data[0]])
+        curs.execute(db_change("select id from topic where code = ? order by id + 0 desc limit 1"), [data[0]])
         t_data = curs.fetchall()
 
         div += '''
-            <h2><a href="/thread/''' + data[0] + '">' + t_str_num + '. ' + data[1] + '''</a></h2>
-            <div id="topic_pre_''' + t_str_num + '''"></div>
-            <div id="topic_back_pre_''' + t_str_num + '''"></div>
+            <h2><a href="/thread/''' + data[0] + '">' + data[0] + '. ' + data[1] + '''</a></h2>
+            <div id="topic_pre_''' + data[0] + '''"></div>
+            <div id="topic_back_pre_''' + data[0] + '''"></div>
             <script>
-                topic_list_load(''' + data[0] + ', 1, "topic_pre_' + t_str_num + '''");
+                new_topic_load(''' + data[0] + ', 0, 1, "?num=1", "topic_pre_' + data[0] + '''");
                 if(''' + t_data[0][0] + ''' !== 1) {
-                    topic_list_load(''' + data[0] + ', ' + t_data[0][0] + ', "topic_back_pre_' + t_str_num + '''");
+                    new_topic_load(''' + data[0] + ', 0, 1, "?num=' + t_data[0][0] + '", "topic_back_pre_' + data[0] + '''");
                 }
             </script>
         '''
