43edce02eed1b2cd495d5153bd4f3ff9ba020dcd
2du <min08101@naver.com>
2021-05-31 19:31:01 +0900

    fix edit filter bug

--- route/inter_wiki_plus.py
+++ route/inter_wiki_plus.py
@@ -125,6 +125,9 @@ def inter_wiki_plus_2(conn, tools, name):
                 <input value="''' + html.escape(value[2]) + '''" type="text" name="icon">
             '''
         elif tools == 'plus_edit_filter':
+            if not name:
+                name = 'test'
+            
             curs.execute(db_change("select plus, plus_t from html_filter where html = ? and kind = 'regex_filter'"), [name])
             exist = curs.fetchall()
             if exist:
