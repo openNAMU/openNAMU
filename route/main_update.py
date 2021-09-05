5e370011683e66b5b9cff9e8bea088663fbc95ed
2du <min08101@naver.com>
2021-09-05 14:03:58 +0000

    mod some

--- route/main_update.py
+++ route/main_update.py
@@ -20,7 +20,7 @@ def main_update_2(conn, r_ver):
             ok = []
 
             ok += [os.system('git remote rm origin')]
-            ok += [os.system('git remote add origin https://github.com/2DU/opennamu.git')]
+            ok += [os.system('git remote add origin https://github.com/opennamu/opennamu.git')]
             ok += [os.system('git fetch origin ' + up_data)]
             ok += [os.system('git reset --hard origin/' + up_data)]
             if (ok[0] and ok[1] and ok[2] and ok[3]) == 0:
@@ -29,7 +29,7 @@ def main_update_2(conn, r_ver):
                 print('Error : update failed')
         elif platform.system() == 'Windows':
             os.system('rd /s /q route')
-            urllib.request.urlretrieve('https://github.com/2DU/opennamu/archive/' + up_data + '.zip', 'update.zip')
+            urllib.request.urlretrieve('https://github.com/opennamu/opennamu/archive/' + up_data + '.zip', 'update.zip')
             zipfile.ZipFile('update.zip').extractall('')
             ok = os.system('xcopy /y /s /r opennamu-' + up_data + ' .')
             if ok == 0:
