a8162dfa6e36a33bc9a2d7f9602c321d3bb5692a
doda <ddark.kr@gmail.com>
2021-09-22 03:51:14 +0900

    fix: github 관련 레거시 url 변경

--- route/main_update.py
+++ route/main_update.py
@@ -50,7 +50,7 @@ def main_update_2(conn, r_ver):
                     <li>''' + load_lang('version') + ' : ' + r_ver + '''</li>
                     <li id="ver_send" style="display: none;">''' + load_lang('lastest') + ''' : </li>
                 </ul>
-                <a href="https://github.com/2du/openNAMU">(Beta)</a> <a href="https://github.com/2du/openNAMU/tree/stable">(Stable)</a>
+                <a href="https://github.com/openNAMU/openNAMU">(Beta)</a> <a href="https://github.com/openNAMU/openNAMU/tree/stable">(Stable)</a>
                 <hr class=\"main_hr\">
                 <form method="post">
                     <button type="submit">''' + load_lang('update') + '''</button>
