f07e60112f2f3f8cd57ee167dd67e57d7c8dc6db
Denevola <81607108+Denevola@users.noreply.github.com>
2022-02-16 11:12:15 +0900

    사용자 지정 404 페이지가 HTTP 상태 코드 404를 반환하도록 수정

--- route/main_error_404.py
+++ route/main_error_404.py
@@ -2,6 +2,6 @@ from .tool.func import *
 
 def main_error_404(e):
     if os.path.exists('404.html') and flask.request.path != '/':
-        return open('404.html', encoding='utf8').read()
+        return open('404.html', encoding='utf8').read(), 404
     else:
         return redirect('/w/' + wiki_set(2))
