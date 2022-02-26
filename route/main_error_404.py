86bfc94d56d8197b49ebc94fbbcb3c22258404b5
surplus-dev <min08101@naver.com>
2022-02-26 16:07:04 +0900

    fix bug

--- route/main_error_404.py
+++ route/main_error_404.py
@@ -1,7 +1,7 @@
 from .tool.func import *
 
-def main_error_404(e):
+def main_error_404():
     if os.path.exists('404.html') and flask.request.path != '/':
-        return open('404.html', encoding='utf8').read(), 404
+        return open('404.html', encoding = 'utf8').read(), 404
     else:
         return redirect('/w/' + wiki_set(2))
