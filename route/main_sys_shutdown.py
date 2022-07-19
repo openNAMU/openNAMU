208f23a43f36eb9aff87876e14dff6df59765162
2du <min08101@naver.com>
2022-07-19 06:33:50 +0000

    fix bug 2

--- route/main_sys_shutdown.py
+++ route/main_sys_shutdown.py
@@ -1,25 +1,26 @@
 from .tool.func import *
 
 def main_sys_shutdown():
-    if admin_check() != 1:
-        return re_error('/error/3')
+    with get_db_connect() as conn:
+        if admin_check() != 1:
+            return re_error('/error/3')
 
-    if flask.request.method == 'POST':
-        admin_check(None, 'shutdown')
-        
-        conn.commit()
+        if flask.request.method == 'POST':
+            admin_check(None, 'shutdown')
 
-        print('----')
-        print('Shutdown')
+            conn.commit()
 
-        os._exit(os.EX_OK)
-    else:
-        return easy_minify(flask.render_template(skin_check(),
-            imp = [load_lang('wiki_shutdown'), wiki_set(), wiki_custom(), wiki_css([0, 0])],
-            data = '''
-                <form method="post">
-                    <button type="submit">''' + load_lang('shutdown') + '''</button>
-                </form>
-            ''',
-            menu = [['manager', load_lang('return')]]
-        ))
+            print('----')
+            print('Shutdown')
+
+            os._exit(os.EX_OK)
+        else:
+            return easy_minify(flask.render_template(skin_check(),
+                imp = [load_lang('wiki_shutdown'), wiki_set(), wiki_custom(), wiki_css([0, 0])],
+                data = '''
+                    <form method="post">
+                        <button type="submit">''' + load_lang('shutdown') + '''</button>
+                    </form>
+                ''',
+                menu = [['manager', load_lang('return')]]
+            ))
--- route/main_view_file.py
+++ route/main_view_file.py
@@ -2,12 +2,13 @@ from .tool.func import *
 from .main_error_404 import main_error_404
 
 def main_view_file(data = ''):
-    if data == 'robots.txt' and not os.path.exists('robots.txt'):
-        return flask.Response('User-agent: *\nDisallow: /\nAllow: /$\nAllow: /w/', mimetype = 'text/plain')
-    elif os.path.exists(data):
-        if re.search(r'\.txt$', data, flags = re.I):
-            return flask.send_from_directory('./', data, mimetype = 'text/plain')
-        else:
-            return flask.send_from_directory('./', data, mimetype = 'text/xml')
+    with get_db_connect() as conn:
+        if data == 'robots.txt' and not os.path.exists('robots.txt'):
+            return flask.Response('User-agent: *\nDisallow: /\nAllow: /$\nAllow: /w/', mimetype = 'text/plain')
+        elif os.path.exists(data):
+            if re.search(r'\.txt$', data, flags = re.I):
+                return flask.send_from_directory('./', data, mimetype = 'text/plain')
+            else:
+                return flask.send_from_directory('./', data, mimetype = 'text/xml')
 
-    return main_error_404()
+        return main_error_404()
