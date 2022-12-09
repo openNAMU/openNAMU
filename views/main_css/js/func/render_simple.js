39cd4a2d89447e286731c78452d5193d5c022b1c
잉여개발기 (SPDV) <min08101@naver.com>
2022-12-09 16:37:57 +0900

    add simple render

--- views/main_css/js/func/render_simple.js
+++ views/main_css/js/func/render_simple.js
@@ -1,9 +1,3 @@
 let opennamu_do_render_simple_url = [
-    '/manager/1',
-    '/manager',
-    '/other',
-    '/setting/phrase',
-    '/setting/main',
-    '/setting/external',
     '/change/skin_set/main'
 ];
