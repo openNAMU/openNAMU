a3f721452fb8707d5ea6db799dd732494d327406
잉여개발기 (SPDV) <min08101@naver.com>
2023-07-02 18:12:50 +0900

    실수로 파이썬 3.8 고려 안함

--- route/api_bbs_w_comment.py
+++ route/api_bbs_w_comment.py
@@ -6,15 +6,15 @@ def api_bbs_w_comment(sub_code : str = '') -> flask.Response:
         curs : typing.Union[sqlite3.Cursor, pymysql.cursors.Cursor] = conn.cursor()
 
         curs.execute(db_change('select set_name, set_data, set_code, set_id from bbs_data where (set_name = "comment" or set_name = "comment_date" or set_name = "comment_user_id") and set_id = ? order by set_code + 0 asc'), [sub_code])
-        db_data : typing.Optional[list[tuple[str, str, str]]] = curs.fetchall()
+        db_data : typing.Optional[typing.List[typing.Tuple[str, str, str]]] = curs.fetchall()
         if not db_data:
             return flask.jsonify({})
         else:
             temp_id : str = ''
             temp_dict : dict[str, str] = {}
-            temp_list : list[dict[str, str]] = []
+            temp_list : typing.List[dict[str, str]] = []
 
-            for_a : tuple[str, str, str]
+            for_a : typing.Tuple[str, str, str]
             for for_a in db_data:
                 if temp_id != for_a[2]:
                     if temp_dict != {}:
