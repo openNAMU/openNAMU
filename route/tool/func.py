7b4b79f46af26c90fb53536ef15dd7ebb59306ec
잉여개발기 (SPDV) <min08101@naver.com>
2023-07-06 06:29:29 +0900

    다른 모양 리스트 지원 시작
    https://github.com/openNAMU/openNAMU/issues/1790
    https://github.com/openNAMU/openNAMU/issues/1434

--- route/tool/func.py
+++ route/tool/func.py
@@ -1094,7 +1094,7 @@ def wiki_css(data):
     data += ['' for _ in range(0, 3 - len(data))]
     
     data_css = ''
-    data_css_ver = '176'
+    data_css_ver = '177'
     
     # Func JS + Defer
     data_css += '<script src="/views/main_css/js/func/func.js?ver=' + data_css_ver + '"></script>'
