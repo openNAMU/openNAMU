874822ed1d70dc6ce620a734266650b866a85eea
잉여개발기 (SPDV) <min08101@naver.com>
2023-07-20 08:47:57 +0900

    너무 테이블 사이가 딱 붙는 문제 해
    https://github.com/openNAMU/openNAMU/issues/1821

--- route/tool/func_render_namumark.py
+++ route/tool/func_render_namumark.py
@@ -1549,7 +1549,7 @@ class class_do_render_namumark:
                 table_data_end = '<table class="' + table_parameter['class'] + '" style="' + table_parameter['table'] + '">' + table_caption + table_data_end + '</table>'
                 table_data_end = '<div class="table_safe" style="' + table_parameter['div'] + '">' + table_data_end + '</div>'
 
-                self.render_data = re.sub(table_regex, lambda x : ('\n<front_br>' + table_data_end + '<back_br>\n'), self.render_data, 1)
+                self.render_data = re.sub(table_regex, lambda x : ('\n<front_br>' + table_data_end + '\n'), self.render_data, 1)
 
             table_count_all -= 1
     
