d869ac5411d2b9be8c803c95beec277008da6814
잉여개발기 (SPDV) <min08101@naver.com>
2023-07-25 18:25:48 +0900

    리스타트 부분 수정

--- route/main_sys_restart.py
+++ route/main_sys_restart.py
@@ -11,15 +11,14 @@ def main_sys_restart():
             print('----')
             print('Restart')
 
-            try:
-                os.execl(sys.executable, sys.executable, *sys.argv)
-            except:
-                pass
-
-            try:
-                os.execl(sys.executable, '"' + sys.executable + '"', *sys.argv)
-            except:
-                return re_error('/error/33')
+            run_list = [sys.executable, 'python' + python_ver, 'python3', 'python']
+            for exe_name in run_list:
+                try:
+                    os.execl(exe_name, os.path.abspath(__file__), *sys.argv)
+                except:
+                    pass
+            
+            return re_error('/error/33')
         else:
             return easy_minify(flask.render_template(skin_check(),
                 imp = [load_lang('wiki_restart'), wiki_set(), wiki_custom(), wiki_css([0, 0])],
