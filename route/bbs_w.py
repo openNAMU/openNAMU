e4000b5412b08bbdb75d87e1d5bd7cf283e6a964
잉여개발기 (SPDV) <min08101@naver.com>
2023-08-28 02:44:29 +0900

    MySQL에서 오작동하던 문제 해결

--- route/bbs_w.py
+++ route/bbs_w.py
@@ -25,7 +25,7 @@ def bbs_w(bbs_num = ''):
         temp_id = ''
         temp_dict = {}
 
-        curs.execute(db_change('select set_name, set_data, set_code from bbs_data where set_id = ? order by set_code + 0 desc'), [bbs_num])
+        curs.execute(db_change('select set_name, set_data, set_code, set_id from bbs_data where set_id = binary ? order by set_code + 0 desc'), [bbs_num])
         db_data = curs.fetchall()
         db_data = list(db_data) if db_data else []
 
@@ -39,7 +39,7 @@ def bbs_w(bbs_num = ''):
                     curs.execute(db_change('select set_data from bbs_data where set_name = "comment_date" and (set_id = ? or set_id like ?) order by set_data desc limit 1'), [bbs_num_str + '-' + temp_dict['code'], bbs_num_str + '-' + temp_dict['code'] + '-%'])
                     db_data = curs.fetchall()
                     last_comment_date = db_data[0][0] if db_data else '0'
-                    
+
                     data += '''
                         <tr>
                             <td>''' + ip_pas(temp_dict['user_id']) + '''</td>
