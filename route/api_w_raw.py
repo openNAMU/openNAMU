3ec3ab8d134e7ce560f8febdb3aecc30d282de2a
잉여개발기 (SPDV) <min08101@naver.com>
2023-10-03 08:15:25 +0000

    temp

--- route/api_w_raw.py
+++ route/api_w_raw.py
@@ -1,26 +1,30 @@
 from .tool.func import *
 
-def api_raw(name = 'Test'):
+def api_w_raw(name = 'Test', rev = '', exist_check = ''):
     with get_db_connect() as conn:
         curs = conn.cursor()
 
-        if acl_check(name, 'render') != 1:
-            rev = flask.request.args.get('num', '')
+        if exist_check != '':
+            curs.execute(db_change("select title from data where title = ?"), [name])
+            if data:
+                return flask.jsonify({ 'exist' : '1' })
+            else:
+                return flask.jsonify({})
+        else:
             if rev != '':
                 curs.execute(db_change("select data from history where title = ? and id = ?"), [name, rev])
             else:
                 curs.execute(db_change("select data from data where title = ?"), [name])
+
             data = curs.fetchall()
             if data:
-                json_data = {
+                return flask.jsonify({
                     "title" : name, 
                     "data" : render_set(
                         doc_name = name, 
                         doc_data = data[0][0],
                         data_type = 'raw'
                     )
-                }
-
-                return flask.jsonify(json_data)
-
-        return flask.jsonify({})
+                })
+            else:
+                return flask.jsonify({})
