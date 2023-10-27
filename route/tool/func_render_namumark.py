6521e382ec25d3b6fffea5905c714ce834a8d2d2
잉여개발기 (SPDV) <min08101@naver.com>
2023-10-28 00:00:31 +0900

    사소한 최적화

--- route/tool/func_render_namumark.py
+++ route/tool/func_render_namumark.py
@@ -149,8 +149,8 @@ class class_do_render_namumark:
 
             storage_count -= 1
 
-        data = re.sub(r'<front_br>', '', data)
-        data = re.sub(r'<back_br>', '', data)
+        data = data.replace('<front_br>', '')
+        data = data.replace('<back_br>', '')
 
         return data
 
@@ -771,14 +771,14 @@ class class_do_render_namumark:
         self.render_data = re.sub(r'\[([^[\]]+)\]', do_render_macro_single, self.render_data)
 
         # macro safe restore
-        self.render_data = re.sub(r'<macro>', '[', self.render_data)
-        self.render_data = re.sub(r'<\/macro>', ']', self.render_data)
+        self.render_data = self.render_data.replace('<macro>', '[')
+        self.render_data = self.render_data.replace('</macro>', ']')
 
     def do_render_math(self):
         def do_render_math_sub(match):
             data = match.group(1)
 
-            data = re.sub(r'\n', '', data)
+            data = data.replace('\n', '')
             data = self.get_tool_data_revert(data)
 
             data_html = self.get_tool_js_safe(data)
@@ -879,7 +879,7 @@ class class_do_render_namumark:
 
                         link_main = self.get_tool_data_restore(link_main, do_type = 'slash')
                         link_main = html.unescape(link_main)
-                        link_main = re.sub(r'"', '&quot;', link_main)
+                        link_main = link_main.replace('"', '&quot;')
                         
                         link_exist = ''
                         file_out = 1
@@ -1088,9 +1088,9 @@ class class_do_render_namumark:
                     link_title = link_main
                     link_main = html.unescape(link_main)
 
-                    link_main = re.sub(r'"', '&quot;', link_main)
-                    link_main = re.sub(r'<', '&lt;', link_main)
-                    link_main = re.sub(r'>', '&gt;', link_main)
+                    link_main = link_main.replace('"', '&quot;')
+                    link_main = link_main.replace('<', '&lt;')
+                    link_main = link_main.replace('>', '&gt;')
 
                     # sub not exist -> sub = main
                     if link_data[1]:
@@ -1443,9 +1443,7 @@ class class_do_render_namumark:
                 # under page & fix url
                 link_main = self.get_tool_link_fix(link_main, 'redirect')
             else:
-                link_inter_name = re.search(link_inter_regex, link_main)
-                link_inter_name = link_inter_name.group(1)
-
+                link_inter_name = inter_check.group(1)
                 link_main = re.sub(link_inter_regex, '', link_main)
 
             # sharp
@@ -1782,7 +1780,7 @@ class class_do_render_namumark:
                         data_revert = self.get_tool_data_revert(html_data)
                         data_revert = re.sub(r'^\n', '', data_revert)
                         data_revert = re.sub(r'\n$', '', data_revert)
-                        data_revert = re.sub(r'&amp;nbsp;', '&nbsp;', data_revert)
+                        data_revert = data_revert.replace('&amp;nbsp;', '&nbsp;')
 
                         self.render_data_js += 'opennamu_do_render_html("' + self.doc_include + 'opennamu_wiki_' + str(html_count) + '");\n'
 
@@ -2220,8 +2218,8 @@ class class_do_render_namumark:
         self.render_data = re.sub(r'\n##[^\n]+', '\n<front_br>', self.render_data)
 
     def do_render_last(self):
-        self.render_data = re.sub(r'<no_br>', '\n', self.render_data)
-        self.render_data = re.sub(r'<no_td>', '||', self.render_data)
+        self.render_data = self.render_data.replace('<no_br>', '\n')
+        self.render_data = self.render_data.replace('<no_td>', '||')
 
         # add category
         if self.doc_include == '':
@@ -2231,23 +2229,23 @@ class class_do_render_namumark:
                 category_set_data = get_main_skin_set(self.curs, self.flask_session, 'main_css_category_set', self.ip)
                 if category_set_data == 'bottom':
                     if re.search(r'<footnote_category>', self.render_data):
-                        self.render_data = re.sub(r'<footnote_category>', '<hr><' + data_name + '></' + data_name + '>', self.render_data, 1)
+                        self.render_data = self.render_data.replace('<footnote_category>', '<hr><' + data_name + '></' + data_name + '>', 1)
                     else:
                         self.render_data += '<hr><' + data_name + '></' + data_name + '>'
                 else:
-                    self.render_data = re.sub(r'<footnote_category>', '', self.render_data, 1)
+                    self.render_data = self.render_data.replace('<footnote_category>', '', 1)
                     self.render_data = '<' + data_name + '></' + data_name + '><hr class="main_hr">' + self.render_data
             else:
-                self.render_data = re.sub(r'<footnote_category>', '', self.render_data, 1)
+                self.render_data = self.render_data.replace(r'<footnote_category>', '', 1)
         else:
-            self.render_data = re.sub(r'<footnote_category>', '', self.render_data, 1)
+            self.render_data = self.render_data.replace(r'<footnote_category>', '', 1)
 
         # remove front_br and back_br
         self.render_data = re.sub(r'\n?<front_br>', '', self.render_data)
         self.render_data = re.sub(r'<back_br>\n?', '', self.render_data)
         
         # \n to <br>
-        self.render_data = re.sub(r'\n', '<br>', self.render_data)
+        self.render_data = self.render_data.replace('\n', '<br>')
 
         # <render_n> restore
         self.render_data = self.get_tool_data_restore(self.render_data)
@@ -2300,7 +2298,8 @@ class class_do_render_namumark:
             toc_data_on = 0
 
             toc_data = re.search(toc_search_regex, self.render_data)
-            toc_data = toc_data.group(1)
+            toc_data = toc_data.group(1) if toc_data else ''
+
             self.data_toc = toc_data
             self.data_toc = re.sub(r'<toc_inside>((?:(?!<toc_inside>|<\/toc_inside>).)*)<\/toc_inside>', do_render_last_toc, self.data_toc)
 
@@ -2308,25 +2307,21 @@ class class_do_render_namumark:
 
             self.render_data = re.sub(toc_search_regex, '', self.render_data)
             if toc_set_data == 'off':
-                self.render_data = re.sub(r'<toc_need_part>', '', self.render_data)
+                self.render_data = self.render_data.replace('<toc_need_part>', '')
             else:
                 if re.search(r'<toc_need_part>', self.render_data):
                     toc_data_on = 1
 
-                self.render_data = re.sub(r'<toc_need_part>', lambda x : (self.data_toc), self.render_data, 20)
-                self.render_data = re.sub(r'<toc_need_part>', '', self.render_data)
+                self.render_data = self.render_data.replace('<toc_need_part>', self.data_toc, 20)
+                self.render_data = self.render_data.replace('<toc_need_part>', '')
 
-            if  self.doc_include != '' or \
-                re.search(r'<toc_no_auto>', self.render_data) or \
-                toc_set_data == 'half_off' or \
-                toc_set_data == 'off' or \
-                toc_data_on == 1:
-                self.render_data = re.sub(r'<toc_no_auto>', '', self.render_data)
+            if self.doc_include != '' or re.search(r'<toc_no_auto>', self.render_data) or toc_set_data == 'half_off' or toc_set_data == 'off' or toc_data_on == 1:
+                self.render_data = self.render_data.replace('<toc_no_auto>', '')
             else:
                 self.render_data = re.sub(r'(?P<in><h[1-6] id="[^"]*">)', '<br>' + self.data_toc + '\\g<in>', self.render_data, 1)
         else:
-            self.render_data = re.sub(r'<toc_need_part>', '', self.render_data)
-            self.render_data = re.sub(r'<toc_no_auto>', '', self.render_data)
+            self.render_data = self.render_data.replace('<toc_need_part>', '')
+            self.render_data = self.render_data.replace('<toc_no_auto>', '')
 
         def do_render_last_footnote(match):
             match = match.group(1)
@@ -2370,8 +2365,8 @@ class class_do_render_namumark:
             self.do_render_last()
             self.data_include = list(reversed(self.data_include))
         else:
-            self.render_data = re.sub(r'\|\|', '<no_td>', self.render_data)
-            self.render_data = re.sub(r'\n', '<no_br>', self.render_data)
+            self.render_data = self.render_data.replace(r'\|\|', '<no_td>')
+            self.render_data = self.render_data.replace('\n', '<no_br>')
 
         return [
             self.render_data, # html
