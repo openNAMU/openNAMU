cd4a9b5b73e690632f78b3b6f8eb81d11afc30e0
잉여개발기 (SPDV) <min08101@naver.com>
2023-10-28 19:35:08 +0900

    미리보기 링크 버그 수정
    https://github.com/openNAMU/openNAMU/issues/1926

--- views/main_css/js/route/editor_sub.js
+++ views/main_css/js/route/editor_sub.js
@@ -1,7 +1,12 @@
 function opennamu_do_editor_preview() {
     var input = document.querySelector('#opennamu_edit_textarea');
     if (input !== null) {
-        fetch("/api/w/test/doc_tool/preview", {
+        var doc_name = 'test';
+        var doc_name_input = document.querySelector('#opennamu_editor_doc_name');
+        if (doc_name_input !== null) {
+            doc_name = doc_name_input.value;
+        }
+        fetch("/api/w_tool/preview/" + opennamu_do_url_encode(doc_name), {
             method: 'POST',
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
             body: new URLSearchParams({
--- views/main_css/js/route/editor_sub.ts
+++ views/main_css/js/route/editor_sub.ts
@@ -1,7 +1,14 @@
 function opennamu_do_editor_preview() {
     const input = document.querySelector('#opennamu_edit_textarea') as HTMLInputElement | null;
     if(input !== null) {
-        fetch("/api/w/test/doc_tool/preview", {
+        let doc_name : string = 'test';
+
+        const doc_name_input = document.querySelector('#opennamu_editor_doc_name') as HTMLInputElement | null;
+        if(doc_name_input !== null) {
+            doc_name = doc_name_input.value;
+        }
+
+        fetch("/api/w_tool/preview/" + (opennamu_do_url_encode(doc_name) as (arg1 : string) => string), {
             method : 'POST',
             headers : { 'Content-Type': 'application/x-www-form-urlencoded' },
             body : new URLSearchParams({
