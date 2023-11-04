afab9c23cc53585ac9b106b55d5e658f09e558ed
잉여개발기 (SPDV) <min08101@naver.com>
2023-11-04 22:21:55 +0900

    코드 최적화 및 버그 수정

--- route/bbs_w.py
+++ route/bbs_w.py
@@ -54,9 +54,6 @@ def bbs_w(bbs_num = '', tool = 'bbs'):
                     <td id="main_table_width">''' + load_lang('last_comment_time') + '''</td>
                 </tr>
         '''
-        
-        temp_id = ''
-        temp_dict = {}
 
         if tool == 'bbs':
             curs.execute(db_change('select set_code, set_id from bbs_data where set_name = "title" and set_id like ? order by set_code + 0 desc'), [bbs_num])
@@ -69,37 +66,30 @@ def bbs_w(bbs_num = '', tool = 'bbs'):
             db_data = curs.fetchall()
             db_data = list(db_data) if db_data else []
 
-            for for_a in db_data + [['', '', '']]:
-                bbs_num_str = for_a[1]
-                if temp_id != for_a[2]:
-                    if temp_id != '':
-                        curs.execute(db_change('select count(*) from bbs_data where set_name = "comment_date" and (set_id = ? or set_id like ?) order by set_code + 0 desc'), [bbs_num_str + '-' + temp_dict['code'], bbs_num_str + '-' + temp_dict['code'] + '-%'])
-                        db_data = curs.fetchall()
-                        comment_count = str(db_data[0][0]) if db_data else '0'
+            temp_dict = { for_a[0] : for_a[1] for for_a in db_data }
 
-                        curs.execute(db_change('select set_data from bbs_data where set_name = "comment_date" and (set_id = ? or set_id like ?) order by set_data desc limit 1'), [bbs_num_str + '-' + temp_dict['code'], bbs_num_str + '-' + temp_dict['code'] + '-%'])
-                        db_data = curs.fetchall()
-                        last_comment_date = db_data[0][0] if db_data else '0'
+            curs.execute(db_change('select count(*) from bbs_data where set_name = "comment_date" and (set_id = ? or set_id like ?) order by set_code + 0 desc'), [for_b[1] + '-' + for_b[0], for_b[1] + '-' + for_b[0] + '-%'])
+            db_data = curs.fetchall()
+            comment_count = str(db_data[0][0]) if db_data else '0'
 
-                        data += '''
-                            <tr>
-                                <td>''' + ip_pas(temp_dict['user_id']) + '''</td>
-                                <td>''' + temp_dict['date'] + '''</td>
-                                <td>''' + last_comment_date + '''</td>
-                            </tr>
-                            <tr>
-                                <td colspan="3">
-                                    <a href="/bbs/w/''' + bbs_num_str + '/' + temp_dict['code'] + '">' + html.escape(temp_dict['title']) + '''</a> 
-                                    (''' + comment_count + ''') 
-                                </td>
-                            </tr>
-                        '''
+            curs.execute(db_change('select set_data from bbs_data where set_name = "comment_date" and (set_id = ? or set_id like ?) order by set_data desc limit 1'), [for_b[1] + '-' + for_b[0], for_b[1] + '-' + for_b[0] + '-%'])
+            db_data = curs.fetchall()
+            last_comment_date = db_data[0][0] if db_data else '0'
 
-                    temp_id = for_a[2]
-                    temp_dict['code'] = for_a[2]
-
-                temp_dict[for_a[0]] = for_a[1]
-
+            data += '''
+                <tr>
+                    <td>''' + ip_pas(temp_dict['user_id']) + '''</td>
+                    <td>''' + temp_dict['date'] + '''</td>
+                    <td>''' + last_comment_date + '''</td>
+                </tr>
+                <tr>
+                    <td colspan="3">
+                        <a href="/bbs/w/''' + for_b[1] + '/' + for_b[0] + '">' + html.escape(temp_dict['title']) + '''</a> 
+                        (''' + comment_count + ''') 
+                    </td>
+                </tr>
+            '''
+                
         data += '</table>'
 
         return easy_minify(flask.render_template(skin_check(),
