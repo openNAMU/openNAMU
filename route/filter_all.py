63f3f5d0a9c9bee771643f44da2949d9e28cf5bd
잉여개발기 (SPDV) <min08101@naver.com>
2023-11-20 11:23:18 +0900

    inter_wiki 파트 조금 수정

--- route/filter_all.py
+++ route/filter_all.py
@@ -1,6 +1,6 @@
 from .tool.func import *
 
-def filter_inter_wiki(tool):
+def filter_all(tool):
     with get_db_connect() as conn:
         curs = conn.cursor()
 
@@ -17,35 +17,30 @@ def filter_inter_wiki(tool):
 
         if tool == 'inter_wiki':
             title = load_lang('interwiki_list')
-
             curs.execute(db_change("select html, plus, plus_t from html_filter where kind = 'inter_wiki'"))
         elif tool == 'email_filter':
             title = load_lang('email_filter_list')
-
             curs.execute(db_change("select html, plus, plus_t from html_filter where kind = 'email'"))
         elif tool == 'name_filter':
             title = load_lang('id_filter_list')
-
             curs.execute(db_change("select html, plus, plus_t from html_filter where kind = 'name'"))
         elif tool == 'edit_filter':
             title = load_lang('edit_filter_list')
-
             curs.execute(db_change("select html, plus, plus_t from html_filter where kind = 'regex_filter'"))
         elif tool == 'file_filter':
             title = load_lang('file_filter_list')
-
             curs.execute(db_change("select html, plus, plus_t from html_filter where kind = 'file'"))
         elif tool == 'image_license':
             title = load_lang('image_license_list')
-
             curs.execute(db_change("select html, plus, plus_t from html_filter where kind = 'image_license'"))
         elif tool == 'extension_filter':
             title = load_lang('extension_filter_list')
-
             curs.execute(db_change("select html, plus, plus_t from html_filter where kind = 'extension'"))
+        elif tool == 'document':
+            title = load_lang('document_filter_list')
+            curs.execute(db_change("select html, plus, plus_t from html_filter where kind = 'document'"))
         else:
             title = load_lang('edit_tool_list')
-
             curs.execute(db_change("select html, plus, plus_t from html_filter where kind = 'edit_top'"))
 
         db_data = curs.fetchall()
@@ -55,21 +50,18 @@ def filter_inter_wiki(tool):
 
             div += html.escape(data[0])
             if admin == 1:
-                if tool in ('inter_wiki', 'edit_filter'):
-                    div += ' <a href="/' + tool + '/add/' + url_pas(data[0]) + '">(' + load_lang('edit') + ')</a>'
+                if tool in ('inter_wiki', 'edit_filter', 'document'):
+                    div += ' <a href="/filter/' + tool + '/add/' + url_pas(data[0]) + '">(' + load_lang('edit') + ')</a>'
                     
-                div += ' <a href="/' + tool + '/del/' + url_pas(data[0]) + '">(' + load_lang('delete') + ')</a>'
+                div += ' <a href="/filter/' + tool + '/del/' + url_pas(data[0]) + '">(' + load_lang('delete') + ')</a>'
 
             div += '</td>'
 
             if tool == 'inter_wiki':
                 div += '<td><a class="opennamu_link_out" href="' + html.escape(data[1]) + '">' + html.escape(data[1]) + '</a></td>'
-            else:
-                div += '<td>' + html.escape(data[1]) + '</td>'
-
-            if tool == 'inter_wiki':
                 div += '<td>' + data[2] + '</td>'
             else:
+                div += '<td>' + html.escape(data[1]) + '</td>'
                 div += '<td>' + html.escape(data[2]) + '</td>'
             
             div += '</tr>'
