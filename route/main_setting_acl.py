dce08d5a7495a2d4725c45c19c0bbdc24bd98aa5
잉여개발기 (SPDV) <min08101@naver.com>
2023-12-09 19:58:45 +0900

    https://github.com/openNAMU/openNAMU/issues/1530

--- route/main_setting_acl.py
+++ route/main_setting_acl.py
@@ -15,54 +15,43 @@ def main_setting_acl():
             8 : 'document_move_acl',
             9 : 'document_delete_acl',
             10 : 'slow_edit_acl',
-            11 : 'edit_bottom_compulsion_acl'
+            11 : 'edit_bottom_compulsion_acl',
+            12 : 'recaptcha_pass_acl',
+            13 : 'recaptcha_one_check_five_pass_acl'
+        }
+        default_list = {
+            12 : 'user'
         }
 
         if flask.request.method == 'POST':
             if admin_check(None, 'edit_set (acl)') != 1:
                 return re_error('/ban')
             else:
-                for i in i_list:
-                    curs.execute(db_change("update other set data = ? where name = ?"), [
-                        flask.request.form.get(i_list[i], 'normal'),
-                        i_list[i]
-                    ])
-
+                curs.executemany(db_change("update other set data = ? where name = ?"), [[flask.request.form.get(i_list[for_a], 'normal'), i_list[for_a]] for for_a in i_list])
                 conn.commit()
 
                 return redirect('/setting/acl')
         else:
             d_list = {}
+            disable = 'disabled' if admin_check() != 1 else ''
+            acl_div = ['' for _ in range(0, len(i_list))]
 
-            if admin_check() != 1:
-                disable = 'disabled'
-            else:
-                disable = ''
-
-            for i in i_list:
-                curs.execute(db_change('select data from other where name = ?'), [i_list[i]])
+            for for_a in i_list:
+                curs.execute(db_change('select data from other where name = ?'), [i_list[for_a]])
                 sql_d = curs.fetchall()
                 if sql_d:
-                    d_list[i] = sql_d[0][0]
+                    d_list[for_a] = sql_d[0][0]
                 else:
-                    curs.execute(db_change('insert into other (name, data, coverage) values (?, ?, "")'), [i_list[i], 'normal'])
-                    d_list[i] = 'normal'
+                    default_data = 'normal' if not for_a in default_list else default_list[for_a]
+                    curs.execute(db_change('insert into other (name, data, coverage) values (?, ?, "")'), [i_list[for_a], default_data])
+                    d_list[for_a] = default_data
 
             conn.commit()
 
-            acl_div = []
-            for i in range(0, len(i_list)):
-                acl_div += ['']
-
             acl_list = get_acl_list()
-            for i in range(0, len(i_list)):
+            for for_a in range(0, len(i_list)):
                 for data_list in acl_list:
-                    if data_list == d_list[i + 1]:
-                        check = 'selected="selected"'
-                    else:
-                        check = ''
-
-                    acl_div[i] += '<option value="' + data_list + '" ' + check + '>' + (data_list if data_list != '' else 'normal') + '</option>'
+                    acl_div[for_a] += '<option value="' + data_list + '" ' + ('selected="selected"' if data_list == d_list[for_a + 1] else '') + '>' + (data_list if data_list != '' else 'normal') + '</option>'
 
             return easy_minify(flask.render_template(skin_check(),
                 imp = [load_lang('main_acl_setting'), wiki_set(), wiki_custom(), wiki_css([0, 0])],
@@ -89,11 +78,11 @@ def main_setting_acl():
                         <h2>''' + load_lang('upload_acl') + '''</h2>
                         <select ''' + disable + ''' name="upload_acl">''' + acl_div[2] + '''</select>
                         
+                        <h3>''' + load_lang('many_upload_acl') + '''</h3>
+                        <select ''' + disable + ''' name="many_upload_acl">''' + acl_div[4] + '''</select>
+                        
                         <h2>''' + load_lang('view_acl') + '''</h2>
                         <select ''' + disable + ''' name="all_view_acl">''' + acl_div[3] + '''</select>
-                        
-                        <h2>''' + load_lang('many_upload_acl') + '''</h2>
-                        <select ''' + disable + ''' name="many_upload_acl">''' + acl_div[4] + '''</select>
                         
                         <h2>''' + load_lang('vote_acl') + '''</h2>
                         <select ''' + disable + ''' name="vote_acl">''' + acl_div[5] + '''</select>
@@ -103,6 +92,12 @@ def main_setting_acl():
 
                         <h2>''' + load_lang('edit_bottom_compulsion_acl') + '''</h2>
                         <select ''' + disable + ''' name="edit_bottom_compulsion_acl">''' + acl_div[10] + '''</select>
+
+                        <h2>''' + load_lang('recaptcha_pass_acl') + '''</h2>
+                        <select ''' + disable + ''' name="recaptcha_pass_acl">''' + acl_div[11] + '''</select>
+
+                        <h3>''' + load_lang('recaptcha_one_check_five_pass_acl') + '''</h3>
+                        <select ''' + disable + ''' name="recaptcha_one_check_five_pass_acl">''' + acl_div[12] + '''</select>
                         
                         <hr class="main_hr">
                         <button id="opennamu_save_button" type="submit">''' + load_lang('save') + '''</button>
