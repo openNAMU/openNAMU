64b69bcc136cd20b8fcd371130164f8a0de3f14a
잉여개발기 (SPDV) <min08101@naver.com>
2023-12-23 20:29:22 +0900

    텐시에도 반영 및 스킨 버전 업데이트

--- views/tenshi/js/skin_set.js
+++ views/tenshi/js/skin_set.js
@@ -3,11 +3,18 @@ function ringo_do_regex_data(data) {
 }
 
 function ringo_get_post() {
-    check = document.getElementById('invert');
+    const check = document.getElementById('invert');
     if(check.checked === true) {
         document.cookie = 'main_css_darkmode=1; path=/';
     } else {
         document.cookie = 'main_css_darkmode=0; path=/';
+    }
+
+    const check_2 = document.getElementById('use_sys_darkmode');
+    if(check_2.checked === true) {
+        document.cookie = 'main_css_use_sys_darkmode=1; path=/';
+    } else {
+        document.cookie = 'main_css_use_sys_darkmode=0; path=/';
     }
 
     history.go(0);
@@ -15,15 +22,29 @@ function ringo_get_post() {
 
 function ringo_do_skin_set() {
     let cookies = document.cookie;
+    if(!cookies.match(ringo_do_regex_data('main_css_use_sys_darkmode')) || (cookies.match(ringo_do_regex_data('main_css_use_sys_darkmode')) && cookies.match(ringo_do_regex_data('main_css_use_sys_darkmode'))[1] === '1')) {
+        console.log('test');
+        if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            document.cookie = 'main_css_darkmode=1; path=/';
+        } else {
+            document.cookie = 'main_css_darkmode=0; path=/';
+        }
+    }
+}
+
+function ringo_load_skin_set() {
+    let cookies = document.cookie;
     
     if(window.location.pathname === '/change/skin_set') {
         let set_language = {
             "en-US" : {
                 "save" : "Save",
-                "darkmode" : "Darkmode"
+                "darkmode" : "Darkmode",
+                "use_sys_darkmode" : "Use system darkmode set",
             }, "ko-KR" : {
                 "save" : "저장",
-                "darkmode" : "다크모드"
+                "darkmode" : "다크모드",
+                "use_sys_darkmode" : "시스템 다크모드 설정 사용",
             }
         }
 
@@ -39,14 +60,17 @@ function ringo_do_skin_set() {
 
         let set_data = {};
 
-        if(
-            cookies.match(ringo_do_regex_data('main_css_darkmode')) &&
-            cookies.match(ringo_do_regex_data('main_css_darkmode'))[1] === '1'
-        ) {
+        if(cookies.match(ringo_do_regex_data('main_css_darkmode')) && cookies.match(ringo_do_regex_data('main_css_darkmode'))[1] === '1') {
             set_data["invert"] = "checked";
         }
 
+        if(!cookies.match(ringo_do_regex_data('main_css_use_sys_darkmode')) || (cookies.match(ringo_do_regex_data('main_css_use_sys_darkmode')) && cookies.match(ringo_do_regex_data('main_css_use_sys_darkmode'))[1] === '1')) {
+            set_data["use_sys_darkmode"] = "checked";
+        }
+
         document.getElementById("main_skin_set").innerHTML = ' \
+            <input ' + set_data["use_sys_darkmode"] + ' type="checkbox" id="use_sys_darkmode" name="use_sys_darkmode" value="use_sys_darkmode"> ' + set_language[language]['use_sys_darkmode'] + ' \
+            <hr class="main_hr"> \
             <input ' + set_data["invert"] + ' type="checkbox" id="invert" name="invert" value="invert"> ' + set_language[language]['darkmode'] + ' \
             <hr class="main_hr"> \
             <button onclick="ringo_get_post();">' + set_language[language]['save'] + '</button> \
@@ -55,3 +79,4 @@ function ringo_do_skin_set() {
 }
 
 window.addEventListener('DOMContentLoaded', ringo_do_skin_set);
+window.addEventListener('DOMContentLoaded', ringo_load_skin_set);
