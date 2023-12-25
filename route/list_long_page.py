924ca9f543a3a87e5444aeb6f2b62ba202f3266a
잉여개발기 (SPDV) <min08101@naver.com>
2023-12-25 23:23:53 +0900

    https://github.com/openNAMU/openNAMU/issues/1209

--- route/list_long_page.py
+++ route/list_long_page.py
@@ -6,20 +6,24 @@ def list_long_page(tool = 'long_page', arg_num = 1):
 
         sql_num = (arg_num * 50 - 50) if arg_num * 50 > 0 else 0
 
-        curs.execute(db_change('select data from other where name = "count_all_title"'))
-        if int(curs.fetchall()[0][0]) > 30000:
-            return re_error('/error/25')
-
         div = '<ul class="opennamu_ul">'
         select_data = 'desc' if tool == 'long_page' else 'asc'
         title = 'long_page' if tool == 'long_page' else 'short_page'
 
-        curs.execute(db_change("select title, length(data) from data order by length(data) " + select_data + " limit ?, 50"), [sql_num])
-        db_data = curs.fetchall()
-        for data in db_data:
-            div += '<li>' + str(data[1]) + ' | <a href="/w/' + url_pas(data[0]) + '">' + html.escape(data[0]) + '</a></li>'
+        curs.execute(db_change("select doc_name, set_data from data_set where set_name = 'length' and doc_rev = '' order by set_data + 0 " + select_data + " limit ?, 50"), [sql_num])
+        n_list = curs.fetchall()
+        for data in n_list:
+            div += '<li>'
+            div += data[1] + ' | <a href="/w/' + url_pas(data[0]) + '">' + html.escape(data[0]) + '</a>'
+            
+            curs.execute(db_change("select set_data from data_set where doc_name = ? and set_name = 'doc_type'"), [data[0]])
+            db_data = curs.fetchall()
+            if db_data and db_data[0][0] != '':
+                div += ' | ' + db_data[0][0]
 
-        div += '</ul>' + next_fix('/list/document/' + ('long' if title == 'long_page' else 'short') + '/', arg_num, db_data)
+            div += '</li>'
+
+        div += '</ul>' + next_fix('/list/document/' + ('long' if title == 'long_page' else 'short') + '/', arg_num, n_list)
 
         return easy_minify(flask.render_template(skin_check(),
             imp = [load_lang(title), wiki_set(), wiki_custom(), wiki_css([0, 0])],
