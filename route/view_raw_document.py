247d4c391c87dbb1ea81c126fe8181693099777a
잉여개발기 (SPDV) <min08101@naver.com>
2024-02-14 00:45:48 +0900

    raw js와 api로 기능 이전

---
+++ route/view_raw_document.py
@@ -0,0 +1,42 @@
+from .tool.func import *
+
+def view_raw_document(name = '', rev = '', doc_acl = ''):
+    with get_db_connect() as conn:
+        curs = conn.cursor()
+
+        rev_str = str(rev)
+
+        sub = '(' + load_lang('raw') + ')'
+        sub += ' (' + rev_str + ')' if rev != '' else ''
+
+        if rev != '':
+            menu = [['history_tool/' + rev_str + '/' + url_pas(name), load_lang('return')]]
+        else:
+            menu = [['w/' + url_pas(name), load_lang('return')]]
+
+        p_data = ''
+        p_data += '''
+            <div id="opennamu_preview_area">
+                <textarea id="opennamu_editor_doc_name" style="display: none;">''' + html.escape(name) + '''</textarea>
+                <textarea id="opennamu_editor_rev" style="display: none;">''' + rev_str + '''</textarea>
+                <button id="opennamu_preview_button" type="button" onclick="opennamu_do_editor_preview('raw');">''' + load_lang('preview') + '''</button>
+                <hr class="main_hr">
+                <textarea readonly id="opennamu_edit_textarea" class="opennamu_textarea_500"></textarea>
+                <script>opennamu_view_raw_document();</script>
+            </div>
+        '''
+        
+        if doc_acl == 'on':
+            p_data = '' + \
+                load_lang('authority_error') + \
+                '<hr class="main_hr">' + \
+                p_data
+            ''
+            
+            sub = ' (' + load_lang('edit') + ')'
+
+        return easy_minify(flask.render_template(skin_check(),
+            imp = [name, wiki_set(), wiki_custom(), wiki_css([sub, 0])],
+            data = p_data,
+            menu = menu
+        ))
