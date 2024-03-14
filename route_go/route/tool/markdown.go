d8707991d8e94f00bbb3491ed1ba92d447ebc2b6
잉여개발기 (SPDV) <min08101@naver.com>
2024-03-14 23:42:03 +0900

    마크다운 HTML 필터 추가

--- route_go/route/tool/markdown.go
+++ route_go/route/tool/markdown.go
@@ -3,13 +3,15 @@ package tool
 import (
 	"database/sql"
 
+	"github.com/microcosm-cc/bluemonday"
 	"github.com/russross/blackfriday/v2"
 )
 
 func Markdown(db *sql.DB, db_set map[string]string, data map[string]string) map[string]interface{} {
 	input := []byte(data["data"])
 
-	html := blackfriday.Run(input)
+	unsafe := blackfriday.Run(input)
+	html := bluemonday.UGCPolicy().SanitizeBytes(unsafe)
 
 	end_data := make(map[string]interface{})
 	end_data["data"] = string(html)
