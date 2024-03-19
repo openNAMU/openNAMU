ce816f831c5f2e96e57d53aad3dff879d1bfb71b
잉여개발기 (SPDV) <min08101@naver.com>
2024-03-19 17:44:16 +0900

    view 권한 없는 경우 이미지도 안 나오게
    https://github.com/openNAMU/openNAMU/issues/2126

--- route/main_view_image.py
+++ route/main_view_image.py
@@ -3,13 +3,16 @@ from .main_func_error_404 import main_func_error_404
 
 def main_view_image(name = ''):
     with get_db_connect() as conn:
-        name = re.sub(r'\.cache_v(?:[0-9]+)$', '', name)
-        mime_type = re.search(r'([^.]+)$', name)
-        if mime_type:
-            mime_type = mime_type.group(1).lower()
-            if mime_type == 'svg':
-                mime_type = 'svg+xml'
+        if acl_check(conn, '', 'render') != 1:
+            name = re.sub(r'\.cache_v(?:[0-9]+)$', '', name)
+            mime_type = re.search(r'([^.]+)$', name)
+            if mime_type:
+                mime_type = mime_type.group(1).lower()
+                if mime_type == 'svg':
+                    mime_type = 'svg+xml'
 
-            return flask.send_from_directory('./' + load_image_url(conn), name, mimetype = 'image/' + mime_type)
+                return flask.send_from_directory('./' + load_image_url(conn), name, mimetype = 'image/' + mime_type)
+            else:
+                return main_func_error_404()
         else:
-            return main_func_error_404()
+            return re_error(conn, '/ban')
