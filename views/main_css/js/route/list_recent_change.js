5110b98675edce739feeb53f64cedd40282620c9
잉여개발기 <min08101@naver.com>
2024-03-22 16:43:15 +0900

    깜박한 부분 추가

--- views/main_css/js/route/list_recent_change.js
+++ views/main_css/js/route/list_recent_change.js
@@ -1,7 +1,13 @@
 "use strict";
 
 function opennamu_list_recent_change() {
-    fetch('/api/lang/tool').then(function(res) {
+    let lang_data = new FormData();
+    lang_data.append('data', 'tool normal edit move delete revert new_doc edit_request user_document')
+
+    fetch('/api/lang', {
+        method : 'post',
+        body : lang_data,
+    }).then(function(res) {
         return res.json();
     }).then(function(lang) {
         lang = lang["data"];
@@ -23,7 +29,25 @@ function opennamu_list_recent_change() {
                 })
             */
 
-            let data_html = '<ul class="opennamu_ul">';
+            let data_html = '';
+
+            let option_list = [
+                ['normal', lang[1]],
+                ['edit', lang[2]],
+                ['move', lang[3]],
+                ['delete', lang[4]],
+                ['revert', lang[5]],
+                ['r1', lang[6]],
+                ['edit_request', lang[7]],
+                ['user', lang[8]]
+            ];
+            for(let for_a = 0; for_a < option_list.length; for_a++) {
+                data_html += '<a href="/recent_change/1/' + option_list[for_a][0] + '">(' + option_list[for_a][1] + ')</a> ';
+            }
+
+            data_html += '<hr class="main_hr">'
+
+            data_html += '<ul class="opennamu_ul">';
             for(let for_a = 0; for_a < data.length; for_a++) {
                 let doc_name = opennamu_do_url_encode(data[for_a][1]);
 
