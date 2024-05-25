01c7b31ef9e2cf29f85dd44006190c615a09d29b
잉여개발기 (SPDV) <min08101@naver.com>
2024-05-25 19:55:14 +0900

    특정 API CORS 정책 변경
    https://github.com/openNAMU/openNAMU/issues/2227

--- route/go_api_list_recent_change.py
+++ route/go_api_list_recent_change.py
@@ -22,4 +22,9 @@ def api_list_recent_change(db_set, num = 1, set_type = 'normal', limit = 10, leg
 
     data = data.decode('utf8')
 
-    return flask.Response(response = data, status = 200, mimetype = 'application/json')
+    response = flask.Response(response = data, status = 200, mimetype = 'application/json')
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "Content-Type")
+    response.headers.add('Access-Control-Allow-Methods', "GET")
+
+    return response
