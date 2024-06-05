96aa67b5297d36a3999099873581b3a3278d32f6
잉여개발기 (SPDV) <min08101@naver.com>
2024-06-06 04:41:59 +0900

    https://github.com/openNAMU/openNAMU/issues/2238

---
+++ views/main_css/js/route/main_sys_restart.js
@@ -0,0 +1,23 @@
+function opennamu_main_sys_restart() {
+    let lang_data = new FormData();
+    lang_data.append('data', 'restart');
+    
+    fetch('/api/lang', {
+        method : 'post',
+        body : lang_data,
+    }).then(function(res) {
+        return res.json();
+    }).then(function(lang) {
+        lang = lang["data"];
+
+        document.getElementById('opennamu_main_sys_restart').innerHTML = '<button id="opennamu_main_sys_restart_button">' + lang[0] + '</button>';
+
+        document.getElementById('opennamu_main_sys_restart_button').addEventListener('click', function() {
+            fetch('/restart', {
+                method: 'post'
+            }).catch(function(e) {
+                window.location.href = '/';
+            });
+        });
+    });
+}
