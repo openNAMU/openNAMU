4eda8c22f7597eeea8f2391783a4a03a829b7fe7
잉여개발기 (SPDV) <min08101@naver.com>
2024-06-10 08:29:22 +0900

    잠재적 버그 수정

--- views/main_css/js/route/wiki_access_password.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-function opennamu_do_wiki_access() {
-    let password = document.getElementById('wiki_access').value;
-    
-    document.cookie = 'opennamu_wiki_access=' + encodeURIComponent(password) + '; path=/;';
-    
-    history.go(0);
-}
