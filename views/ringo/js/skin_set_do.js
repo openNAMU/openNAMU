58dff43f826cd9ba0c5cf50cf23e1f630ab254d8
잉여개발기 (SPDV) <min08101@naver.com>
2024-07-02 15:21:38 +0900

    눈뽕 개선용 신형 스크립트 적용
    https://github.com/openNAMU/openNAMU/issues/2313

---
+++ views/ringo/js/skin_set_do.js
@@ -0,0 +1,56 @@
+function ringo_do_regex_data(data) {
+    return new RegExp('(?:^|; )' + data + '=([^;]*)');
+}
+
+function ringo_do_skin_set() {
+    let cookies = document.cookie;
+
+    if(!window.localStorage.getItem('main_css_use_sys_darkmode') || window.localStorage.getItem('main_css_use_sys_darkmode') === '1') {
+        if(cookies.match(ringo_do_regex_data('main_css_darkmode')) && cookies.match(ringo_do_regex_data('main_css_darkmode'))[1] === '1') {
+            if(!window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                document.cookie = 'main_css_darkmode=0; path=/';
+                history.go(0);
+            }
+        }
+        
+        if(!cookies.match(ringo_do_regex_data('main_css_darkmode')) || cookies.match(ringo_do_regex_data('main_css_darkmode'))[1] === '0') {
+            if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                document.cookie = 'main_css_darkmode=1; path=/';
+                history.go(0);
+            }
+        }
+    }
+
+    if(window.localStorage.getItem('main_css_off_sidebar') && window.localStorage.getItem('main_css_off_sidebar') === '1') {
+        document.getElementById('ringo_add_style').innerHTML += `
+            section {
+                width: auto;
+                display: block;
+                margin: auto;
+            }
+
+            .do_fixed {
+                display: none;
+            }
+        `;
+    }
+
+    if(window.localStorage.getItem('main_css_fixed_width') && window.localStorage.getItem('main_css_fixed_width') !== '') {
+        let fixed_width_data = window.localStorage.getItem('main_css_fixed_width');
+        document.getElementById('ringo_add_style').innerHTML += `
+            article.main {
+                max-width: ` + fixed_width_data + `px;
+            }
+        `;
+    }
+
+    if(window.localStorage.getItem('main_css_sidebar_right') && window.localStorage.getItem('main_css_sidebar_right') === '1') {
+        document.getElementById('ringo_add_style').innerHTML += `
+            .do_fixed {
+                float: right;
+            }
+        `;
+    }
+}
+
+ringo_do_skin_set();
