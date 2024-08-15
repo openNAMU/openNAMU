a32bb8750061708b5aeb122d3b4ce277d95017bd
잉여개발기 <min08101@naver.com>
2024-08-16 02:20:43 +0900

    임시

--- route_go/route/api_w_comment.go
+++ route_go/route/api_w_comment.go
@@ -17,64 +17,70 @@ func Api_w_comment(call_arg []string) string {
 	db := tool.DB_connect()
 	defer db.Close()
 
-	page_int, err := strconv.Atoi(other_set["num"])
-	if err != nil {
-		log.Fatal(err)
-	}
+	return_data := make(map[string]interface{})
 
-	if page_int > 0 {
-		page_int = (page_int * 50) - 50
+	if !tool.Check_acl(db, "", "", "comment_view", other_set["ip"]) {
+		return_data["response"] = "require auth"
 	} else {
-		page_int = 0
-	}
+		return_data["response"] = "ok"
 
-	stmt, err := db.Prepare(tool.DB_change("select set_name, doc_rev, set_data from data_set where (set_name = 'comment' or set_name like 'comment%') and doc_name = ? order by doc_rev asc limit ?, 50"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stmt.Close()
-
-	rows, err := stmt.Query(other_set["doc_name"], page_int)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-
-	data_list := []map[string]string{}
-	temp_dict := map[string]string{}
-	before_set_code := ""
-
-	for rows.Next() {
-		var set_name string
-		var set_code string
-		var set_data string
-
-		err := rows.Scan(&set_name, &set_code, &set_data)
+		page_int, err := strconv.Atoi(other_set["num"])
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		if before_set_code != set_code {
-			if before_set_code != "" {
-				data_list = append(data_list, temp_dict)
-			}
-
-			temp_dict = map[string]string{}
-			temp_dict["id"] = set_code
-
-			before_set_code = set_code
+		if page_int > 0 {
+			page_int = (page_int * 50) - 50
+		} else {
+			page_int = 0
 		}
 
-		temp_dict[set_name] = set_data
-	}
+		stmt, err := db.Prepare(tool.DB_change("select set_name, doc_rev, set_data from data_set where (set_name = 'comment' or set_name like 'comment%') and doc_name = ? order by doc_rev asc limit ?, 50"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer stmt.Close()
 
-	if before_set_code != "" {
-		data_list = append(data_list, temp_dict)
-	}
+		rows, err := stmt.Query(other_set["doc_name"], page_int)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
 
-	return_data := make(map[string]interface{})
-	return_data["response"] = "ok"
-	return_data["data"] = data_list
+		data_list := []map[string]string{}
+		temp_dict := map[string]string{}
+		before_set_code := ""
+
+		for rows.Next() {
+			var set_name string
+			var set_code string
+			var set_data string
+
+			err := rows.Scan(&set_name, &set_code, &set_data)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if before_set_code != set_code {
+				if before_set_code != "" {
+					data_list = append(data_list, temp_dict)
+				}
+
+				temp_dict = map[string]string{}
+				temp_dict["id"] = set_code
+
+				before_set_code = set_code
+			}
+
+			temp_dict[set_name] = set_data
+		}
+
+		if before_set_code != "" {
+			data_list = append(data_list, temp_dict)
+		}
+
+		return_data["data"] = data_list
+	}
 
 	json_data, _ := json.Marshal(return_data)
 	return string(json_data)
--- route_go/route/api_w_comment_post.go
+++ route_go/route/api_w_comment_post.go
@@ -22,41 +22,48 @@ func Api_w_comment_post(call_arg []string) string {
 	db := tool.DB_connect()
 	defer db.Close()
 
-	// ip := other_set["ip"]
-
-	stmt, err := db.Prepare(tool.DB_change("select doc_rev from data_set where set_name = 'comment' and doc_name = ? order by doc_rev asc limit 1"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stmt.Close()
-
-	var view_count string
-
-	err = stmt.QueryRow(other_set["doc_name"]).Scan()
-	if err != nil {
-		if err == sql.ErrNoRows {
-			view_count = "0"
-		} else {
-			log.Fatal(err)
-		}
-	}
-
-	view_count_int, _ := strconv.Atoi(view_count)
-	view_count_int += 1
-
-	stmt, err = db.Prepare(tool.DB_change("insert into data_set (doc_name, doc_rev, set_name, set_data) values (?, ?, ?, ?)"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stmt.Close()
-
-	_, err = stmt.Exec(other_set["doc_name"], view_count)
-	if err != nil {
-		log.Fatal(err)
-	}
+	other_set["comment_user_id"] = other_set["ip"]
 
 	return_data := make(map[string]interface{})
-	return_data["response"] = "ok"
+
+	if !tool.Check_acl(db, "", "", "comment_write", other_set["ip"]) {
+		return_data["response"] = "require auth"
+	} else {
+		return_data["response"] = "ok"
+
+		stmt, err := db.Prepare(tool.DB_change("select doc_rev from data_set where set_name = 'comment' and doc_name = ? order by doc_rev asc limit 1"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer stmt.Close()
+
+		var view_count string
+
+		err = stmt.QueryRow(other_set["doc_name"]).Scan()
+		if err != nil {
+			if err == sql.ErrNoRows {
+				view_count = "0"
+			} else {
+				log.Fatal(err)
+			}
+		}
+
+		view_count_int, _ := strconv.Atoi(view_count)
+		view_count_int += 1
+
+		for _, v := range Comment_need_list() {
+			stmt, err = db.Prepare(tool.DB_change("insert into data_set (doc_name, doc_rev, set_name, set_data) values (?, ?, ?, ?)"))
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer stmt.Close()
+
+			_, err = stmt.Exec(other_set["doc_name"], view_count, v, other_set[v])
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
 
 	json_data, _ := json.Marshal(return_data)
 	return string(json_data)
