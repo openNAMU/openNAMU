8782078e88a51c729af6dfd3394a31d8ad73a939
잉여개발기 <min08101@naver.com>
2026-07-12 23:55:47 +0900

    setting 추가

---
+++ route/view_setting.go
@@ -0,0 +1,43 @@
+package route
+
+import "opennamu/route/tool"
+
+func View_setting(config tool.Config) string {
+	db := tool.DB_connect()
+	defer tool.DB_close(db)
+
+    set_list := [][]string{
+        { "main", tool.Get_language(db, "main_setting", true) },
+        { "phrase", tool.Get_language(db, "text_setting", true) },
+        { "robot", "robots.txt" },
+        { "external", tool.Get_language(db, "ext_api_req_set", true) },
+        { "head", tool.Get_language(db, "main_head", true) },
+        { "body/top", tool.Get_language(db, "main_body", true) },
+        { "body/bottom", tool.Get_language(db, "main_bottom_body", true) },
+        { "sitemap_set", tool.Get_language(db, "sitemap_management", true) },
+        { "top_menu", tool.Get_language(db, "top_menu_setting", true) },
+        { "skin_set", tool.Get_language(db, "main_skin_set_default", true) },
+        { "404_page", tool.Get_language(db, "404_page_setting", true) },
+    }
+
+    set_data := ""
+    for _, li := range set_list {
+        set_data += `<li><a href="/setting/` +
+            li[0] +
+            `">` +
+            li[1] +
+            `</a></li>`
+    }
+
+	return tool.Get_template(
+        db,
+        config,
+        tool.Get_language(db, "setting", true),
+        set_data,
+        []any{},
+        [][]any{
+            { "manager", tool.Get_language(db, "return", true) },
+        },
+        map[string]string{},
+    )
+}
