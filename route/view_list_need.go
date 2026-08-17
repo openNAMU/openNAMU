2758f94c78c9efe4bfe21e3e75a7d117ee9f7273
spdv <min08101@naver.com>
2026-08-18 00:21:02 +0900

    개선

--- route/view_list_need.go
+++
@@ -1,9 +0,0 @@
-package route
-
-import "opennamu/route/tool"
-
-func View_list_need(config tool.Config) string {
-	db := tool.DB_connect()
-	defer tool.DB_close(db)
-	return list_extra_query(db, config, tool.Get_language(db, "need_document", true), "select title, link from back where type = 'no' order by title")
-}
--- route/view_list_need_page.go
+++ route/view_list_need_page.go
@@ -11,13 +11,17 @@ func View_list_need_page(config tool.Config, page string) string {
 	defer tool.DB_close(db)
 	page_num := list_extra_page_number(page)
 	offset := (page_num - 1) * 50
-	rows := tool.Query_DB(db, "select title, link from back where type = 'no' order by title limit ?, 50", offset)
+	rows := tool.Query_DB(
+		db,
+		"select b.title, count(*) from back b where b.type = 'no' and not exists (select 1 from data d where d.title = b.title) group by b.title order by count(*) desc, b.title asc limit ?, 50",
+		offset,
+	)
 	body := strings.Builder{}
 	count := 0
 	for rows.Next() {
-		name, link := "", ""
-		if rows.Scan(&name, &link) == nil {
-			body.WriteString(tool.Get_list_ui(`<a href="/w/`+tool.Url_parser(name)+`">`+tool.HTML_escape(name)+`</a>`, tool.HTML_escape(link), "", ""))
+		name, link_count := "", ""
+		if rows.Scan(&name, &link_count) == nil {
+			body.WriteString(tool.Get_list_ui(`<a href="/w/`+tool.Url_parser(name)+`">`+tool.HTML_escape(name)+`</a>`, tool.Get_language(db, "link_count", true)+" : "+tool.HTML_escape(link_count), "", ""))
 			count++
 		}
 	}
