fdc783ae034ca213c2772c675ab48ae662f65bff
spdv <min08101@naver.com>
2026-09-09 10:05:37 +0900

    https://github.com/openNAMU/openNAMU/issues/2653

--- route/view_filter.go
+++ route/view_filter.go
@@ -24,8 +24,36 @@ func View_filter(config tool.Config, kind string) string {
 	} else if kind == "replace_filter" {
 		data += `<tr id="main_table_top_tr"><td>` + tool.Get_language(db, "regex", true) + `</td><td>` + tool.Get_language(db, "replacement", true) + `</td></tr>`
 	} else {
-		data += `<tr id="main_table_top_tr">` +
-			`<td id="main_table_width">A</td><td id="main_table_width">B</td><td id="main_table_width">C</td></tr>`
+		header := []string{"name", "", ""}
+		switch kind {
+		case "inter_wiki", "outer_link":
+			header = []string{"name", "link", "icon"}
+		case "document":
+			header = []string{"name", "regex", "acl"}
+		case "edit_filter":
+			header = []string{"name", "regex", "day"}
+		case "template":
+			header = []string{"template", "explanation", ""}
+		case "edit_top":
+			header = []string{"title", "markup", ""}
+		case "email_filter":
+			header = []string{"email", "", ""}
+		case "name_filter", "file_filter":
+			header = []string{"regex", "", ""}
+		case "image_license":
+			header = []string{"license", "", ""}
+		case "extension_filter":
+			header = []string{"extension", "max_file_size", ""}
+		}
+		data += `<tr id="main_table_top_tr">`
+		for _, value := range header {
+			data += `<td id="main_table_width">`
+			if value != "" {
+				data += tool.Get_language(db, value, true)
+			}
+			data += `</td>`
+		}
+		data += `</tr>`
 	}
 	rows := tool.Get_html_filter_rows(db, spec.db_kind)
 	defer rows.Close()
